a4424ec34fcc3e79e70e54bca9b65ac1fe3d8f1e
satamas <se.atamas@gmail.com>
2017-04-17 22:20:39 +0300

    Serve static correctly in prod.

--- le_francais/settings/production.py
+++ le_francais/settings/production.py
@@ -3,6 +3,7 @@ from __future__ import absolute_import, unicode_literals
 from .base import *
 
 DEBUG = False
+STATICFILES_STORAGE = 'whitenoise.django.GzipManifestStaticFilesStorage'
 
 try:
     from .local import *
