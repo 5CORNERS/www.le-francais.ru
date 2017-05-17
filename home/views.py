f7496b758d1e128f271839c8861fa3696a5a3a08
satamas <se.atamas@gmail.com>
2017-05-18 00:11:48 +0300

    Fix redirect uri

--- home/views.py
+++ home/views.py
@@ -16,11 +16,11 @@ from home.src.site_import import load_old_site_pages, LessonInformation, add_new
 
 flow = OAuth2WebServerFlow(client_id='499129759772-bqrp9ha0vfibn6t76fdgdmd87khnn2e0.apps.googleusercontent.com',
                            client_secret='CRTqrmLi-116OMgpFOnYS6wH',
-                           scope='https://www.googleapis.com/auth/drive',
-                           redirect_uri='http://localhost:8000/import/authorized')
+                           scope='https://www.googleapis.com/auth/drive')
 
 
 def authorize(request):
+    flow.redirect_uri = 'http://' + request.get_host() + '/import/authorized'
     return redirect(flow.step1_get_authorize_url())
 
 
