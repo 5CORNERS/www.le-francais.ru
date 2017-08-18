449c71960655317202d74cb931554400e41cdc73
satamas <semyon.atamas@jetbrains.com>
2017-08-18 23:56:41 +0300

    Update sidebar navigation.

--- home/static/js/le_francais.js
+++ home/static/js/le_francais.js
@@ -1,16 +1,19 @@
+function getNavRootId() {
+  return $('meta[name="nav-root-id"]').attr("data-value")
+}
+
 $(document).ready(function () {
-
-    $.getJSON('/api/nav/?rootId=11', function (navData) {
-       $('#sidebar').treeview({
-         enableLinks: true,
-         data: navData
-       });
+  $.getJSON('/api/nav/?rootId=' + getNavRootId(), function (navData) {
+    $('#sidebar').treeview({
+      enableLinks: true,
+      data: navData
     });
-    $(".sidebar-collapse-button").click(function () {
-        var $target = $($(this).data("target"));
-        $target.toggleClass('in');
-        $(this).attr('aria-expanded', $target.hasClass('in'));
-    });
+  });
+  $(".sidebar-collapse-button").click(function () {
+    var $target = $($(this).data("target"));
+    $target.toggleClass('in');
+    $(this).attr('aria-expanded', $target.hasClass('in'));
+  });
 
-    $('audio').audioPlayer();
+  $('audio').audioPlayer();
 });
