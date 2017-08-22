5dc71176fc49529a9bc0fbd61680dbc253437883
ILYA DUmov <ilia.dumov@gmail.com>
2017-08-22 18:53:29 +0300

    migration fix

--- le_francais/urls.py
+++ le_francais/urls.py
@@ -11,6 +11,12 @@ from wagtail.wagtaildocs import urls as wagtaildocs_urls
 from home.urls import site_import_urls, api_urls
 from search import views as search_views
 
+from pybb.views import ProfileEditView
+from home.views import Search, MovePostView, AorAddPostView, AorEditPostView, AorTopicView, move_post_processing
+from home.forms import AORProfileForm, RegistrationFormCaptcha
+from profiles.views import UserTopics, UserPosts, safe_logout
+from registration.backends.default.views import RegistrationView
+
 urlpatterns = [
     url(r'^django-admin/', include(admin.site.urls)),
     url(r'^login/$', auth_views.login, name='login'),
@@ -25,7 +31,26 @@ urlpatterns = [
     url(r'^import/', include(site_import_urls)),
     url(r'^api/', include(api_urls)),
 
+    url(r'^accounts/register/$', RegistrationView.as_view(form_class=RegistrationFormCaptcha),
+        name="registration_register"),
+    url(r'^accounts/logout/$', safe_logout, name='auth_logout'),
+    url(r'^accounts/', include('registration.backends.default.urls')),
+    url(r'^accounts/', include('django.contrib.auth.urls')),
+
+    url(r'^forum/profile/edit/$', ProfileEditView.as_view(form_class=AORProfileForm), name='pybb:edit_profile'),
+    url(r'^forum/users/(?P<username>[^/]+)/topics/$', UserTopics.as_view(),
+        name='user_topics'),
+    url(r'^forum/users/(?P<username>[^/]+)/posts/$', UserPosts.as_view(),
+        name='user_posts'),
+
+    url(r'^forum/topic/(?P<pk>\d+)/$', AorTopicView.as_view(), name='topic'),
+    url(r'^forum/topic/(?P<pk>\d+)/move/$', MovePostView.as_view(), name='move_post'),
+    url(r'^forum/forum/(?P<forum_id>\d+)/topic/add/$', AorAddPostView.as_view(), name='add_topic'),
+    url(r'^forum/topic/(?P<topic_id>\d+)/post/add/$', AorAddPostView.as_view(), name='add_post'),
+    url(r'^forum/post/(?P<pk>\d+)/edit/$', AorEditPostView.as_view(), name='edit_post'),
+    url(r'^forum/post/move/processing/$', move_post_processing, name='move_post_processing'),
     url(r'^forum/', include('pybb.urls', namespace='pybb')),
+
     url(r'^', include('social_django.urls', namespace='social')),
     url(r'^', include(wagtail_urls)),
 
