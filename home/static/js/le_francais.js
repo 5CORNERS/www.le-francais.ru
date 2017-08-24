8bd277550a53a37944d0625a1a42e38f2618f7ca
satamas <semyon.atamas@jetbrains.com>
2017-08-24 13:32:07 +0300

    Update tabs.

--- home/static/js/le_francais.js
+++ home/static/js/le_francais.js
@@ -34,7 +34,7 @@ function addExpandedStateToNavdata(navData) {
         }
       })
     }
-    if(node.state && node.state.selected) {
+    if (node.state && node.state.selected) {
       node.state.expanded = true;
     }
   });
@@ -65,4 +65,16 @@ $(document).ready(function () {
   });
 
   $('audio').audioPlayer();
+
+  // Javascript to enable link to tab
+  var url = document.location.toString();
+  if (url.match('#')) {
+    $('.nav-tabs a[href="#' + url.split('#')[1] + '"]').tab('show');
+  }
+  $('.nav-tabs a:first').tab('show');
+
+// Change hash for page-reload
+  $('.nav-tabs a').on('shown.bs.tab', function (e) {
+    window.location.hash = e.target.hash;
+  })
 });
