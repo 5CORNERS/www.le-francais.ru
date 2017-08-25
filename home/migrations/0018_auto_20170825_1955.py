eb0ccdf5776d4902fbab2e01d074004d4e59fb2b
AntonDumov <anton.dumov@gmail.com>
2017-08-25 20:21:30 +0300

    Update 0018_auto_20170825_1955.py

--- home/migrations/0018_auto_20170825_1955.py
+++ home/migrations/0018_auto_20170825_1955.py
@@ -8,7 +8,7 @@ from django.db import migrations, models
 class Migration(migrations.Migration):
 
     dependencies = [
-        ('home', '0001_initial'),
+        ('home', '0017_auto_20170822_1908'),
     ]
 
     operations = [
