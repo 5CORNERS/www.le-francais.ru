88d383e2aa3499ca55d59aefaf63a6a97525162a
satamas <semyon.atamas@jetbrains.com>
2017-09-03 19:36:22 +0300

    Add download button to audioplayer.

--- home/blocks/AudioBlock.py
+++ home/blocks/AudioBlock.py
@@ -1,9 +1,10 @@
 from wagtail.wagtailcore import blocks
-from wagtail.wagtailcore.blocks.field_block import URLBlock
+from wagtail.wagtailcore.blocks.field_block import URLBlock, BooleanBlock
 
 
 class AudioBlock(blocks.StructBlock):
     url = URLBlock()
+    downloadable = BooleanBlock(required=False)
 
     class Meta:
         template = 'blocks/audio_player.html'
