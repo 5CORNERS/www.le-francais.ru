1c8f6795f148bd35651e9c92c7793e7107feae87
AntonDumov <anton.dumov@gmail.com>
2017-09-20 14:31:17 +0300

    custom user_model and registration fixed

--- custom_user/models.py
+++ custom_user/models.py
@@ -1,12 +1,55 @@
-from django.contrib.auth.models import AbstractUser
+from django.contrib.auth.models import AbstractUser, UserManager
+from django.core.mail import send_mail
+from django.db import models
 from django.utils.translation import ugettext_lazy as _
-from django.db.models import EmailField
+
+
+class CustomUserManager(UserManager):
+	def _create_user(self, username, email, password, **extra_fields):
+		"""
+		Creates and saves a User with the given username, email and password.
+		"""
+		if not email:
+			raise ValueError('Email is required')
+		if not username:
+			raise ValueError('The given username must be set')
+		email = self.normalize_email(email)
+		username = self.model.normalize_username(username)
+		user = self.model(username=username, email=email, **extra_fields)
+		user.set_password(password)
+		user.save(using=self._db)
+		return user
+
+	def create_user(self, username, email, password=None, **extra_fields):
+		extra_fields.setdefault('is_staff', False)
+		extra_fields.setdefault('is_superuser', False)
+		return self._create_user(username, email, password, **extra_fields)
 
 
 class User(AbstractUser):
-	email = EmailField(_('email address'), unique=True, error_messages={
-		'unique': _("A user with that email adress already exists."),
-	}, )
+	email = models.EmailField(
+		verbose_name='email address',
+		max_length=255,
+		unique=True,
+		error_messages={
+			'unique': _("A user with that email already exists."),
+		},
+	)
+	username = models.CharField(
+		max_length=32,
+		verbose_name='nickname',
+		help_text='Your forum nickname (can be changed later)',
+		unique=True,
+		error_messages={
+			'unique': _("A user with that nickname already exists."),
+		},
+	)
+	objects = CustomUserManager()
 	USERNAME_FIELD = 'email'
-	EMAIL_FIELD = 'email'
 	REQUIRED_FIELDS = ['username']
+
+	def email_user(self, subject, message, from_email=None, **kwargs):
+		"""
+		Sends an email to this User.
+		"""
+		send_mail(subject, message, from_email, [self.email], **kwargs)
