bf25076807dc73e403b424ca9a013be72c8831bd
AntonDumov <anton.dumov@gmail.com>
2017-10-13 17:42:06 +0300

    profile forms

--- home/forms.py
+++ home/forms.py
@@ -1,10 +1,12 @@
-from django.contrib.auth.forms import AuthenticationForm
+from captcha.fields import CaptchaField
 from django import forms
+from django.contrib.auth.forms import AuthenticationForm
 from django.utils.translation import ugettext as _
 from pybb.forms import EditProfileForm
 from registration.forms import RegistrationFormUniqueEmail
+
 from profiles.models import Profile
-from captcha.fields import CaptchaField
+
 
 class CaptchaTestForm(forms.Form):
     captcha = CaptchaField()
@@ -21,11 +23,11 @@ class AuthenticationFormCaptcha(AuthenticationForm):
 class AORProfileForm(EditProfileForm):
     class Meta:
         model = Profile
-        fields = ('signature', 'date_show_type', 'show_signatures', 'time_zone',
-                  'language', 'avatar', 'icq', 'skype', 'jabber', 'site', 'interests')
+        fields = ('autosubscribe', 'signature', 'show_signatures', 'time_zone',
+                  'language', 'avatar',)
 
     signature = forms.CharField(widget=forms.Textarea, label=_('Signature'),
-        required=False)
+                                required=False)
     # time_zone = forms.ChoiceField()
 
 
