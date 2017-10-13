b2cf5d834a8698c7c2974b4a216641eef081db98
AntonDumov <anton.dumov@gmail.com>
2017-10-13 17:29:04 +0300

    autolink nofollow

--- forum/customextensions/nofollowlinks.py
+++ forum/customextensions/nofollowlinks.py
@@ -5,7 +5,7 @@ Add html attribut to all links nofollow="true"
 """
 
 from markdown.extensions import Extension
-from markdown.inlinepatterns import util, dequote, LinkPattern
+from markdown.inlinepatterns import util, dequote, LinkPattern, Pattern
 
 NOBRACKET = r'[^\]\[]*'
 
@@ -21,6 +21,8 @@ NOIMG = r'(?<!\!)'
 LINK_RE = NOIMG + BRK + \
           r'''\(\s*(<.*?>|((?:(?:\(.*?\))|[^\(\)]))*?)\s*((['"])(.*?)\12\s*)?\)'''
 
+AUTOLINK_RE = r'<((?:[Ff]|[Hh][Tt])[Tt][Pp][Ss]?://[^>]*)>'
+
 
 class NewLinkPattern(LinkPattern):
 	""" Return a link element from the given match. """
@@ -31,7 +33,9 @@ class NewLinkPattern(LinkPattern):
 		title = m.group(13)
 		href = m.group(9)
 		rel = "nofollow"
+		target = "_blank"
 		el.set("rel", rel)
+		el.set("target", target)
 
 		if href:
 			if href[0] == "<":
@@ -46,10 +50,25 @@ class NewLinkPattern(LinkPattern):
 		return el
 
 
+class NewAutolinkPattern(Pattern):
+	""" Return a link Element given an autolink (`<http://example/com>`). """
+
+	def handleMatch(self, m):
+		el = util.etree.Element("a")
+		el.set('href', self.unescape(m.group(2)))
+		el.set('rel', 'nofollow')
+		target = "_blank"
+		el.set("target", target)
+		el.text = util.AtomicString(m.group(2))
+		return el
+
+
 class NoFollowLinksExtension(Extension):
 	def extendMarkdown(self, md, md_globals):
 		link = NewLinkPattern(LINK_RE, md)
+		autolink = NewAutolinkPattern(AUTOLINK_RE, md)
 		md.inlinePatterns['link'] = link
+		md.inlinePatterns['autolink'] = autolink
 
 
 def makeExtension(*args, **kwargs):
