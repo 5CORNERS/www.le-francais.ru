7a57d082eeae41ae517463839d64d1daf6030b3a
AntonDumov <anton.dumov@gmail.com>
2017-10-13 17:41:42 +0300

    environments read from file

--- manage.py
+++ manage.py
@@ -3,11 +3,16 @@ from __future__ import absolute_import, unicode_literals
 
 import os
 import sys
+from dotenv import load_dotenv
 
 sys.tracebacklimit = 30000
+
 if __name__ == "__main__":
-    os.environ.setdefault("DJANGO_SETTINGS_MODULE", "le_francais.settings.dev")
+	os.environ.setdefault("DJANGO_SETTINGS_MODULE", "le_francais.settings.dev")
 
-    from django.core.management import execute_from_command_line
+	from django.core.management import execute_from_command_line
 
-    execute_from_command_line(sys.argv)
+	dotenv_path = os.path.join(os.path.dirname(__file__), '.env')
+	load_dotenv(dotenv_path)
+
+	execute_from_command_line(sys.argv)
