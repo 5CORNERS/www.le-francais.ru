6e15b68a58fb06b44b2236b21f76ba8e88f14a48
AntonDumov <anton.dumov@gmail.com>
2017-10-19 18:09:11 +0300

    added top and recent topics

---
+++ home/templatetags/forum_tags.py
@@ -0,0 +1,22 @@
+from django import template
+from pybb.models import Topic
+
+register = template.Library()
+
+
+@register.inclusion_tag('entries.html')
+def last_topics(count=50, *args, **kwargs):
+    qs = Topic.objects.filter(forum__hidden=False).filter(forum__category__hidden=False)
+    qs = qs.filter(on_moderation=False)
+    qs = qs.order_by('-updated','-created')
+    qs = qs[:count]
+    return dict(object_list=qs, **{'to_end':True})
+
+
+@register.inclusion_tag('entries.html')
+def top_topics(count=50, *args, **kwargs):
+    qs = Topic.objects.filter(forum__hidden=False).filter(forum__category__hidden=False)
+    qs = qs.filter(on_moderation=False)
+    qs = qs.order_by('-post_count')
+    qs = qs[:count]
+    return dict(object_list=qs, **{'to_end':False})
