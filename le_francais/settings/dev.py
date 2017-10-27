2ae3bf756d20d7927b839ceac2ce101417ef945e
AntonDumov <anton.dumov@gmail.com>
2017-10-27 17:24:25 +0300

    sdsad

--- le_francais/settings/dev.py
+++ le_francais/settings/dev.py
@@ -7,20 +7,13 @@ DEBUG = True
 COMPRESS_ENABLED = True
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = '8_83ey7rv$u_@t79klk$mp%#l)sol#uq2(@zj(@*$7o^3dpy21'
+SECRET_KEY = os.environ.get('SECRET_KEY','8_83ey7rv$u_@t79klk$mp%#l)sol#uq2(@zj(@*$7o^3dpy21')
 
 ADMINS = [('semyon', 'semyon@atamas.com')]
 
 # EMAIL_BACKEND = 'django.core.mail.backends.console.EmailBackend'
 EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
 
-DEFAULT_FROM_EMAIL = 'no_reply@files.le-francais.ru'
-EMAIL_HOST = os.environ.get('EMAIL_HOST', "localhost")
-EMAIL_PORT = int(os.environ.get('EMAIL_PORT', 25))
-EMAIL_HOST_USER = os.environ.get('EMAIL_HOST_USER', "")
-EMAIL_HOST_PASSWORD = os.environ.get('EMAIL_HOST_PASSWORD', "")
-EMAIL_USE_TLS =True if (os.environ.get('EMAIL_USE_TLS', False)) == 'True' else False
-EMAIL_USE_SSL =True if (os.environ.get('EMAIL_USE_TLS', False)) == 'True' else False
 
 try:
     from .local import *
