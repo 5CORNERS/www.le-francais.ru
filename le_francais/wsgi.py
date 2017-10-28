a30b5d8fa771b1c487fcc4d3e4daf66df2c2725a
AntonDumov <anton.dumov@gmail.com>
2017-10-28 17:51:03 +0300

    Htmlmin install, run in pruduction mode

--- le_francais/wsgi.py
+++ le_francais/wsgi.py
@@ -14,7 +14,7 @@ import os
 from django.core.wsgi import get_wsgi_application
 from whitenoise.django import DjangoWhiteNoise
 
-os.environ.setdefault("DJANGO_SETTINGS_MODULE", "le_francais.settings.dev")
+os.environ.setdefault("DJANGO_SETTINGS_MODULE", "le_francais.settings.production")
 
 application = get_wsgi_application()
 application = DjangoWhiteNoise(application)
