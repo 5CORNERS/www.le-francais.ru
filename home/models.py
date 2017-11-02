de3afaf025ac5d193430dc610571a6bcbdabffcb
AntonDumov <anton.dumov@gmail.com>
2017-11-02 18:11:39 +0300

    Signature

--- home/models.py
+++ home/models.py
@@ -65,9 +65,11 @@ class IndexReviews(Orderable):
     page = ParentalKey(IndexPage, related_name='reviews')
     url = URLField(null=True, blank=True)
     text = CharField(null=True,blank=True, max_length=1024)
+    signature = CharField(null=True, blank=True, max_length=1024)
     panels = [
         FieldPanel('url'),
         FieldPanel('text'),
+        FieldPanel('signature')
     ]
 
 class DefaultPage(Page):
