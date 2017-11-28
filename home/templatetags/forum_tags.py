139fb378be89810f793f749e0d065a30386c8233
AntonDumov <anton.dumov@gmail.com>
2017-11-28 16:43:49 +0300

    gtm

--- home/templatetags/forum_tags.py
+++ home/templatetags/forum_tags.py
@@ -4,7 +4,7 @@ from pybb.models import Topic
 register = template.Library()
 
 
-@register.inclusion_tag('entries.html')
+@register.inclusion_tag('tags/entries.html')
 def last_topics(count=50, *args, **kwargs):
     qs = Topic.objects.filter(forum__hidden=False).filter(forum__category__hidden=False)
     qs = qs.filter(on_moderation=False)
@@ -13,7 +13,7 @@ def last_topics(count=50, *args, **kwargs):
     return dict(object_list=qs, **{'to_end':True})
 
 
-@register.inclusion_tag('entries.html')
+@register.inclusion_tag('tags/entries.html')
 def top_topics(count=50, *args, **kwargs):
     qs = Topic.objects.filter(forum__hidden=False).filter(forum__category__hidden=False)
     qs = qs.filter(on_moderation=False)
