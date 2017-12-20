fb22a604357042b46edddcf83000a3dc19bd112a
AntonDumov <anton.dumov@gmail.com>
2017-12-20 14:38:07 +0300

    conjugation field renamed

--- conjugation/management/commands/buld.py
+++ conjugation/management/commands/buld.py
@@ -8,14 +8,36 @@ def get_dicts():
         file='C:\\Users\\ilia.dumov\\PycharmProjects\\www.le-francais.ru\\conjugation\\data\\conjugation-fr.xml',
         mode='rb')
 
-    verbs_dict = xmltodict.parse(verbs_xml)
-    conjugation_dict = xmltodict.parse(conjugation_xml)
+    verbs_dict = xmltodict.parse(verbs_xml,dict_constructor=dict)
+    conjugation_dict = xmltodict.parse(conjugation_xml,dict_constructor=dict)
     return verbs_dict, conjugation_dict
 
 
 from django.core.management import BaseCommand
 
+from conjugation.models import Template, Verb
 
 class Command(BaseCommand):
     def handle(self, *args, **options):
-        verbs, conjugation = get_dicts()
+        verbs, templates = get_dicts()
+        for template in templates['conjugation-fr']['template']:
+            Template.objects.create(
+                name=template['@name'],
+                infinitive=template['infinitive'],
+                indicative=template['indicative'],
+                conditional=template['conditional'],
+                subjunctiv=template['subjunctive'],
+                imperative=template['imperative'],
+                participle=template['participle']
+            )
+        for verb in verbs['verbs-fr']['v']:
+            verb_template = Template.objects.get(name=verb['t'])
+            if 'aspirate-h' in verb:
+                aspirate_h = True
+            else:
+                aspirate_h = False
+            Verb.objects.create(
+                infinitive=verb['i'],
+                template=verb_template,
+                aspirated_h = aspirate_h
+            )
