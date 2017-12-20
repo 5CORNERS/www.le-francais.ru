d620b22f1e17899cdee4a73e782105d1e98f39e5
AntonDumov <anton.dumov@gmail.com>
2017-12-20 16:21:36 +0300

    commit

---
+++ home/blocks/AdvertisementBlocks.py
@@ -0,0 +1,8 @@
+from wagtail.wagtailcore.blocks import StructBlock
+from wagtail.wagtailsnippets.blocks import SnippetChooserBlock
+
+from home.models import AdvertisementSnippet
+
+
+class AdvertisementInline(StructBlock):
+    advertisement = SnippetChooserBlock(AdvertisementSnippet)
