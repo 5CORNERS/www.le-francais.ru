cdf373578bf638b649fd1a65694bac860b4ebb5d
AntonDumov <anton.dumov@gmail.com>
2018-02-02 16:57:42 +0300

    inline advertisements disable checkbox

--- home/blocks/AdvertisementBlocks.py
+++ home/blocks/AdvertisementBlocks.py
@@ -1,11 +1,13 @@
 from wagtail.wagtailcore.blocks import StructBlock
 from wagtail.wagtailsnippets.blocks import SnippetChooserBlock
+from wagtail.wagtailcore.blocks import BooleanBlock
 
 from home.models import InlineAdvertisementSnippet
 
 
 class AdvertisementInline(StructBlock):
     advertisement = SnippetChooserBlock(InlineAdvertisementSnippet)
+    disable = BooleanBlock(required=False)
 
     class Meta:
         icon = 'snippet'
