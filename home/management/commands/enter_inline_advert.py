85b97db335b9e2af3c9ab52d9b04de4449e6c6a1
AntonDumov <anton.dumov@gmail.com>
2018-03-06 16:41:03 +0300

    enter_inline_adver

--- home/management/commands/enter_inline_advert.py
+++ home/management/commands/enter_inline_advert.py
@@ -2,51 +2,38 @@ import re
 
 from django.core.management import BaseCommand
 
+from home.models import InlineAdvertisementSnippet
+from home.models import LessonPage
+
+PATH = '''C:\\Users\\ilia.dumov\\PycharmProjects\\www.le-francais.ru\\html_files\\'''
+
 
 class Command(BaseCommand):
     def handle(self, *args, **options):
-        with open('html_files\\' + 'lecon-001' + '.html', 'r', encoding='utf-8') as f:
-            page_body = PageBody(f)
-            print('done!')
-
-def read_all_lecons():
-
-
-class Block:
-    def __init__(self, block_type: str, block_num: int, raw_text=None):
-        self.type = block_type
-        self.n = block_num
-        if raw_text:
-            self.html = re.sub('^<!--BLOCK_([A-Za-z]{4})_([0-9]{2})-->$', '', raw_text, flags=re.M)
-
-
-class PageBody:
-    def __init__(self, file):
-        self.file_lines = list((file))
-        self.blocks = []
-        block_pos = []
-        for line_num, line in enumerate(self.file_lines):
-            match = re.match('<!--BLOCK_([A-Za-z]{4})_([0-9]{2})-->', line)
-            if bool(match):
-                block_pos.append((line_num, int(match.group(2)), match.group(1)))
-        for block_start in block_pos:
-            block_type = block_start[2]
-            block_num = block_start[1]
-            if block_type == 'HTML':
-                line_num = block_start[0] + 1
-                block_raw_text = ''
-                match = False
-                while not match:
-                    block_raw_text += self.file_lines[line_num]
-                    match = re.match('(<!--BLOCK_HTML_[0-9]{2}-->)|(<!--TAB_BODY_END-->)', self.file_lines[line_num])
-                    line_num += 1
-                block = Block(block_type, block_num, block_raw_text)
-            else:
-                block = Block(block_type, block_num)
-            self.blocks.append(block)
-
-    def add_snippets(self):
-        for block in self.blocks:
-            if block.type == 'HTML':
-                html_text = block.html
-                re.match('()()()')
+        for page in LessonPage.objects.all():
+            if page.lesson_number > 5:
+                break
+            new_body = []
+            for stream_child in page.body.__iter__():
+                if stream_child.block_type == 'html':
+                    values = re.split('^<!--INLINE_SNIPPET_([0-9a-zA-Z]+?)-->$', str(stream_child.value), flags=re.M)
+                    for value in values:
+                        if value == '' or re.match('^\s+?$',value,re.M):
+                            continue
+                        elif value == '1' or value == '2' or value == '3':
+                            new_body.append((
+                                'advertisement', {
+                                    'advertisement': InlineAdvertisementSnippet.objects.get(name=value),
+                                    'disable': False
+                                }
+                            ))
+                        else:
+                            new_body.append(('html', value))
+                else:
+                    new_body.append((stream_child.block_type, stream_child.value))
+            page.body = new_body
+            print(page.title + '\t-\tSaving draft',end='')
+            page.save_revision()
+            print('\t-\tPublishing', end='')
+            page.get_latest_revision().publish()
+            print('\t-\tDone')
