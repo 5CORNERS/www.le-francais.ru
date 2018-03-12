a7bb1980c7f50e64641d7f445ce6d416f48b6b16
AntonDumov <anton.dumov@gmail.com>
2018-03-12 17:51:17 +0300

    pdfjs page parameter

--- home/blocks/DocumentViewerBlock.py
+++ home/blocks/DocumentViewerBlock.py
@@ -1,8 +1,9 @@
 from wagtail.wagtailcore import blocks
-from wagtail.wagtailcore.blocks.field_block import URLBlock, CharBlock
+from wagtail.wagtailcore.blocks.field_block import URLBlock, CharBlock, IntegerBlock
 
 class DocumentViewerBlock(blocks.StructBlock):
     url = CharBlock()
+    page = IntegerBlock(required=False)
 
     class Meta:
         template = 'blocks/document.html'
