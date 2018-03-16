42a2d053dfa334d6d388ac43c38dd482a6ab7e1a
anton.dumov@gmail.com <anton.dumov@gmail.com>
2018-03-16 15:50:19 +0300

    hstore to json

--- conjugation/models.py
+++ conjugation/models.py
@@ -1,17 +1,24 @@
 from django.db import models
-from django.contrib.postgres.fields import HStoreField
+from django.contrib.postgres.fields import JSONField
+
 
 # Create your models here.
 class Template(models.Model):
-    name = models.CharField(max_length=100)
-    infinitive = HStoreField()
-    indicative = HStoreField()
-    conditional = HStoreField()
-    subjunctive = HStoreField()
-    imperative = HStoreField()
-    participle = HStoreField()
+    name = models.CharField(max_length=200)
+    infinitive = JSONField()
+    indicative = JSONField()
+    conditional = JSONField()
+    subjunctive = JSONField()
+    imperative = JSONField()
+    participle = JSONField()
+
+    def __str__(self):
+        return self.name
 
 class Verb(models.Model):
     infinitive = models.CharField(max_length=100)
     template = models.ForeignKey(Template)
     aspirate_h = models.BooleanField(default=False)
+    
+    def __str__(self):
+        return self.infinitive
