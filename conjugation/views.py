4298d77485c5bce5599e555ced62d22b8b6e14a9
AntonDumov <anton.dumov@gmail.com>
2018-03-16 18:43:43 +0300

    end day commit 2

--- conjugation/views.py
+++ conjugation/views.py
@@ -1,20 +1,10 @@
-from conjugation.models import Verb
-from django.http import Http404, JsonResponse
-
-from .models import Verb
-
+from conjugation.models import Verb as V
+from django.http import Http404, JsonResponse, HttpResponse
+from .utils import get_conjugations
 
 def get_conjugation(request, verb):
     try:
-        v = Verb.objects.get(infinitive=verb)
-    except Verb.DoesNotExist:
+        v = V.objects.get(infinitive=verb)
+    except V.DoesNotExist:
         raise Http404('Verb does not exist')
-    d = dict(
-        infinitive=v.template.infinitive,
-        indicative=v.template.indicative,
-        conditional=v.template.conditional,
-        subjunctive=v.template.subjunctive,
-        imperative=v.template.imperative,
-        participle=v.template.participle,
-    )
-    return JsonResponse(d)
+    return JsonResponse(get_conjugations(v))
