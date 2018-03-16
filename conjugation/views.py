f4dda27f71cb8dc39f15b369a6e295c552f8542c
AntonDumov <anton.dumov@gmail.com>
2018-03-16 16:42:00 +0300

    added urls and si,ple view

--- conjugation/views.py
+++ conjugation/views.py
@@ -1,3 +1,20 @@
-from django.shortcuts import render
+from conjugation.models import Verb
+from django.http import Http404, JsonResponse
 
-# Create your views here.
+from .models import Verb
+
+
+def get_conjugation(request, verb):
+    try:
+        v = Verb.objects.get(infinitive=verb)
+    except Verb.DoesNotExist:
+        raise Http404('Verb does not exist')
+    d = dict(
+        infinitive=v.template.infinitive,
+        indicative=v.template.indicative,
+        conditional=v.template.conditional,
+        subjunctive=v.template.subjunctive,
+        imperative=v.template.imperative,
+        participle=v.template.participle,
+    )
+    return JsonResponse(d)
