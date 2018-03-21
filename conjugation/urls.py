24561906140b0a9d9b6b381d9c256c71f19f9956
AntonDumov <anton.dumov@gmail.com>
2018-03-21 11:11:12 +0300

    fixes

--- conjugation/urls.py
+++ conjugation/urls.py
@@ -3,6 +3,6 @@ from django.conf.urls import url
 from . import views
 
 urlpatterns = [
-    url(r'^(?P<verb>.+)$', views.get_conjugation)
-    url(r'^parler$' views.get_parler_table)
+    url(r'^(?P<verb>.+)$', views.get_conjugation),
+    url(r'^parler$', views.get_parler_table),
 ]
