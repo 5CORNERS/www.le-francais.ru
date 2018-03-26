fd095bd1f31493f0a7e9921c15c6fccc677d08b3
AntonDumov <anton.dumov@gmail.com>
2018-03-26 18:20:24 +0300

    2603 conjugation commit

--- home/static/js/le_francais.js
+++ home/static/js/le_francais.js
@@ -1,175 +1,175 @@
 function getNavRootId() {
-  return $('meta[name="nav-root-id"]').attr("data-value")
+    return $('meta[name="nav-root-id"]').attr("data-value")
 }
 
 function resizeIframe(obj) {
-            obj.style.height = obj.contentWindow.document.body.scrollHeight + 'px';
-        }
+    obj.style.height = obj.contentWindow.document.body.scrollHeight + 'px';
+}
 
 function getPageId() {
-  return $('meta[name="page-id"]').attr("data-value")
+    return $('meta[name="page-id"]').attr("data-value")
 }
 
 
 function setNodeState(href, isExpanded) {
-  var nodesCollapsedState = localStorage.getItem("nodesCollapsedState") != null ?
-    JSON.parse(localStorage.getItem("nodesCollapsedState")) :
-    {};
-  nodesCollapsedState[href] = isExpanded;
-  localStorage.setItem("nodesCollapsedState", JSON.stringify(nodesCollapsedState))
+    var nodesCollapsedState = localStorage.getItem("nodesCollapsedState") != null ?
+        JSON.parse(localStorage.getItem("nodesCollapsedState")) :
+        {};
+    nodesCollapsedState[href] = isExpanded;
+    localStorage.setItem("nodesCollapsedState", JSON.stringify(nodesCollapsedState))
 }
 
 function addExpandedStateToNavdata(navData) {
-  var state = localStorage.getItem("nodesCollapsedState") != null ?
-    JSON.parse(localStorage.getItem("nodesCollapsedState")) :
-    {};
-  navData.forEach(function (node) {
-    var href = navData.href;
-    if (href in state) {
-      var expanded = state[href];
+    var state = localStorage.getItem("nodesCollapsedState") != null ?
+        JSON.parse(localStorage.getItem("nodesCollapsedState")) :
+        {};
+    navData.forEach(function (node) {
+        var href = navData.href;
+        if (href in state) {
+            var expanded = state[href];
 
-      node.state.expanded = expanded
-    }
-    if (node.nodes) {
-      addExpandedStateToNavdata(node.nodes);
-      node.nodes.forEach(function (child) {
-        if (child.state && (child.state.expanded || child.state.selected)) {
-          node.state.expanded = true;
+            node.state.expanded = expanded
         }
-      })
-    }
-    if (node.state && node.state.selected) {
-      node.state.expanded = true;
-    }
-  });
-  return navData
+        if (node.nodes) {
+            addExpandedStateToNavdata(node.nodes);
+            node.nodes.forEach(function (child) {
+                if (child.state && (child.state.expanded || child.state.selected)) {
+                    node.state.expanded = true;
+                }
+            })
+        }
+        if (node.state && node.state.selected) {
+            node.state.expanded = true;
+        }
+    });
+    return navData
 }
 
 $(document).ready(function () {
-  var navTreeElement = document.getElementById("sidebar");
-  if(navTreeElement != null){
-    $.getJSON('/api/nav/?rootId=' + getNavRootId() + '&pageId=' + getPageId(), function (navData) {
-      navData = addExpandedStateToNavdata(navData);
-      $(navTreeElement).treeview({
-        levels: 0,
-        data: navData,
-        onNodeSelected: function (event, data) {
-          window.location.href = data.href;
-        },
-        onNodeExpanded: function (event, data) {
-          setNodeState(data.href, true)
-        },
-        onNodeCollapsed: function (event, data) {
-          setNodeState(data.href, false)
-        }
-      });
-    });
-    $(".sidebar-collapse-button").click(function () {
-      var $target = $($(this).data("target"));
-      $target.toggleClass('active');
-      $(this).attr('aria-expanded', $target.hasClass('active'));
-    });
-  }
+    var navTreeElement = document.getElementById("sidebar");
+    if (navTreeElement != null) {
+        $.getJSON('/api/nav/?rootId=' + getNavRootId() + '&pageId=' + getPageId(), function (navData) {
+            navData = addExpandedStateToNavdata(navData);
+            $(navTreeElement).treeview({
+                levels: 0,
+                data: navData,
+                onNodeSelected: function (event, data) {
+                    window.location.href = data.href;
+                },
+                onNodeExpanded: function (event, data) {
+                    setNodeState(data.href, true)
+                },
+                onNodeCollapsed: function (event, data) {
+                    setNodeState(data.href, false)
+                }
+            });
+        });
+        $(".sidebar-collapse-button").click(function () {
+            var $target = $($(this).data("target"));
+            $target.toggleClass('active');
+            $(this).attr('aria-expanded', $target.hasClass('active'));
+        });
+    }
 
-  $('audio').audioPlayer();
+    $('audio').audioPlayer();
 
-  // Javascript to enable link to tab
-  var hash = document.location.hash;
-  if (hash != "") {
-    hash = hash.substring(1);
-    $('.nav-tabs a[href="#' + hash + '"]').tab('show');
-  } else {
-    $('.nav-tabs a:first').tab('show');
-  }
+    // Javascript to enable link to tab
+    var hash = document.location.hash;
+    if (hash != "") {
+        hash = hash.substring(1);
+        $('.nav-tabs a[href="#' + hash + '"]').tab('show');
+    } else {
+        $('.nav-tabs a:first').tab('show');
+    }
 
-  window.onhashchange = function () {
-    $('.nav-tabs a[href="#' + location.hash.substring(1) + '"]').tab('show');
-  };
+    window.onhashchange = function () {
+        $('.nav-tabs a[href="#' + location.hash.substring(1) + '"]').tab('show');
+    };
 
 // Change hash for page-reload
-  $('.nav-tabs a').on('shown.bs.tab', function (e) {
-    history.pushState(null, null, e.target.hash);
-  })
+    $('.nav-tabs a').on('shown.bs.tab', function (e) {
+        history.pushState(null, null, e.target.hash);
+    })
 });
 
 (function () {
-'use strict';
-/**
- * Converts details/summary tags into working elements in browsers that don't yet support them.
- * @return {void}
- */
-var details = (function () {
+    'use strict';
+    /**
+     * Converts details/summary tags into working elements in browsers that don't yet support them.
+     * @return {void}
+     */
+    var details = (function () {
 
-  var isDetailsSupported = function () {
-    // https://mathiasbynens.be/notes/html5-details-jquery#comment-35
-    // Detect if details is supported in the browser
-    var el = document.createElement("details");
-    var fake = false;
+        var isDetailsSupported = function () {
+            // https://mathiasbynens.be/notes/html5-details-jquery#comment-35
+            // Detect if details is supported in the browser
+            var el = document.createElement("details");
+            var fake = false;
 
-    if (!("open" in el)) {
-      return false;
-    }
+            if (!("open" in el)) {
+                return false;
+            }
 
-    var root = document.body || function () {
-      var de = document.documentElement;
-      fake = true;
-      return de.insertBefore(document.createElement("body"), de.firstElementChild || de.firstChild);
-    }();
+            var root = document.body || function () {
+                var de = document.documentElement;
+                fake = true;
+                return de.insertBefore(document.createElement("body"), de.firstElementChild || de.firstChild);
+            }();
 
-    el.innerHTML = "<summary>a</summary>b";
-    el.style.display = "block";
-    root.appendChild(el);
-    var diff = el.offsetHeight;
-    el.open = true;
-    diff = diff !== el.offsetHeight;
-    root.removeChild(el);
+            el.innerHTML = "<summary>a</summary>b";
+            el.style.display = "block";
+            root.appendChild(el);
+            var diff = el.offsetHeight;
+            el.open = true;
+            diff = diff !== el.offsetHeight;
+            root.removeChild(el);
 
-    if (fake) {
-      root.parentNode.removeChild(root);
-    }
+            if (fake) {
+                root.parentNode.removeChild(root);
+            }
 
-    return diff;
-  }();
+            return diff;
+        }();
 
-  if (!isDetailsSupported) {
-    var blocks = document.querySelectorAll("details>summary");
-    for (var i = 0; i < blocks.length; i++) {
-      var summary = blocks[i];
-      var details = summary.parentNode;
+        if (!isDetailsSupported) {
+            var blocks = document.querySelectorAll("details>summary");
+            for (var i = 0; i < blocks.length; i++) {
+                var summary = blocks[i];
+                var details = summary.parentNode;
 
-      // Apply "no-details" to for unsupported details tags
-      if (!details.className.match(new RegExp("(\\s|^)no-details(\\s|$)"))) {
-        details.className += " no-details";
-      }
+                // Apply "no-details" to for unsupported details tags
+                if (!details.className.match(new RegExp("(\\s|^)no-details(\\s|$)"))) {
+                    details.className += " no-details";
+                }
 
-      summary.addEventListener("click", function (e) {
-        var node = e.target.parentNode;
-        if (node.hasAttribute("open")) {
-          node.removeAttribute("open");
-        } else {
-          node.setAttribute("open", "open");
+                summary.addEventListener("click", function (e) {
+                    var node = e.target.parentNode;
+                    if (node.hasAttribute("open")) {
+                        node.removeAttribute("open");
+                    } else {
+                        node.setAttribute("open", "open");
+                    }
+                });
+            }
         }
-      });
-    }
-  }
-});
+    });
 
-(function () {
-  var onReady = function onReady(fn) {
-    if (document.addEventListener) {
-      document.addEventListener("DOMContentLoaded", fn);
-    } else {
-      document.attachEvent("onreadystatechange", function () {
-        if (document.readyState === "interactive") {
-          fn();
-        }
-      });
-    }
-  };
+    (function () {
+        var onReady = function onReady(fn) {
+            if (document.addEventListener) {
+                document.addEventListener("DOMContentLoaded", fn);
+            } else {
+                document.attachEvent("onreadystatechange", function () {
+                    if (document.readyState === "interactive") {
+                        fn();
+                    }
+                });
+            }
+        };
 
-  onReady(function () {
-    details();
-  });
-})();
+        onReady(function () {
+            details();
+        });
+    })();
 
 }());
