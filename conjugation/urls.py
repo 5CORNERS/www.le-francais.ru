f62dd38f7023a65db61a59bce4ef80c7315aefd8
AntonDumov <anton.dumov@gmail.com>
2018-04-18 18:18:22 +0300

    none

--- conjugation/urls.py
+++ conjugation/urls.py
@@ -3,7 +3,7 @@ from django.conf.urls import url
 from . import views
 
 urlpatterns = [
-    url(r'verbs_autocomplete/.{0,50}?$', views.get_autocomplete_list),
-    url(r'verb/$', views.get_table, name='conjugation'),
-    url(r'verb/(?P<verb>.{0,50}?)/', views.get_table, name='conjugation_verb'),
+    url(r'verbs_autocomplete/.{0,50}?$', views.get_autocomplete_list, name='autocomplete'),
+    url(r'search/$', views.search, name='search'),
+    url(r'verb/(?P<feminin>feminin_)?(?P<se>se_)?(?P<verb>\w{1,30})', views.get_table, name='verb'),
 ]
