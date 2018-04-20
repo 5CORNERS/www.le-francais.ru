005eee868b4abd1759baa9ac5fe6b61d716e3334
AntonDumov <anton.dumov@gmail.com>
2018-04-20 17:49:07 +0300

    verb_not_found and index templates and views

--- conjugation/urls.py
+++ conjugation/urls.py
@@ -5,5 +5,8 @@ from . import views
 urlpatterns = [
     url(r'verbs_autocomplete/.{0,50}?$', views.get_autocomplete_list, name='autocomplete'),
     url(r'search/$', views.search, name='search'),
-    url(r'verb/(?P<feminin>feminin_)?(?P<se>se_)?(?P<verb>\w{1,30})', views.get_table, name='verb'),
+    url(r'verb/(?P<feminin>feminin_)?(?P<se>se_)?(?P<verb>.{1,30})$', views.verb, name='verb'),
+    url(r'$', views.index),
 ]
+
+
