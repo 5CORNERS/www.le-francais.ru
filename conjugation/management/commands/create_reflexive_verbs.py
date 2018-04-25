0b7d0736e7abc74e80b6d4f61376d693c22c813f
AntonDumov <anton.dumov@gmail.com>
2018-04-25 11:25:19 +0300

    fix rv method

--- conjugation/management/commands/create_reflexive_verbs.py
+++ conjugation/management/commands/create_reflexive_verbs.py
@@ -1,10 +1,13 @@
 from django.core.management import BaseCommand
+
 from conjugation.models import Verb as V, ReflexiveVerb  as RV
+
 
 class Command(BaseCommand):
     def handle(self, *args, **options):
         for v in V.objects.all():
             if v.reflexive:
-                reflexive_verb = RV.objects.create(infinitive=v.reflexive)
-                reflexive_verb.create_no_accents()
-                reflexive_verb.save()
+                rv, created = RV.objects.get_or_create(infinitive=v.reflexive)
+                rv.create_no_accents()
+                rv.save()
+                print(rv.infinitive)
