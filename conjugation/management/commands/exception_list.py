67348382647fae2a1d80ed0b2726d3b7eef7922a
AntonDumov <anton.dumov@gmail.com>
2018-04-26 15:29:12 +0300

    new fields in verb model

--- conjugation/management/commands/exception_list.py
+++ conjugation/management/commands/exception_list.py
@@ -75,8 +75,83 @@ def fill_deffectives():
     print(verb_does_not_exist)
 
 
+def return_true_false(param):
+    if param==True or param==1:
+        return True
+    elif isinstance(param, str):
+        if param.lower()=='true':
+            return True
+        else:
+            return False
+    else:
+        return False
+
+
 def fill_other_parametres():
     file_path = "conjugation/data/Full verb list.csv"
+    table = read_csv(open(file_path,encoding='utf-8'))
+    dict = table.to_dict()
+    print(0)
+    for i in range(len(dict['VERB'])):
+        if i == 0:
+            continue
+        print(dict["VERB"][i], end='')
+        try:
+            v = V.objects.get(infinitive=dict['VERB'][i])
+        except:
+            print('\t'+"can't find this verb")
+        print("\tfounded")
+        s_en = return_true_false(dict["S'EN"][i])
+        can_passive = return_true_false(dict["CAN BE PASSIVE"][i])
+        can_feminin = return_true_false(dict["CAN BE FEMININ"][i])
+        reflexive_only = return_true_false(dict["PRONOMINAL ONLY"][i])
+        can_reflexive = return_true_false(dict["CAN BE PRONOMINAL"][i])
+        is_second_form = return_true_false(dict["HAVE 2 FORMS OF CONJOUGATION"][i])
+
+        is_frequent = return_true_false(dict["Fréquent"][i])
+        is_transitive = return_true_false(dict["Transitif"][i])
+        is_intransitive = return_true_false(dict["Intransitif"][i])
+        is_pronominal = return_true_false(dict["Intransitif"][i])
+        aspirate_h = return_true_false(dict["*"][i])
+        belgium = return_true_false(dict["BE"][i])
+        africa = return_true_false(dict["Afrique"][i])
+        conjugated_with_avoir = return_true_false(dict["AVOIR"][i])
+        conjugated_with_etre = return_true_false(dict["ÊTRE"][i])
+        is_defective = return_true_false(dict["DEFECTIVE"])
+        is_impersonal = return_true_false(dict["IMPERSONNEL"])
+        book = return_true_false(dict["книжный"][i])
+        is_rare = return_true_false(dict["редкоупотребимый"])
+        is_archaique = return_true_false(dict["устаревший"])
+        is_slang = return_true_false(dict["слэнг"][i])
+        group_no = dict["GROUP No"]
+
+        regle_id = dict["Règle IDX"]
+
+        v.s_en = s_en
+        v.can_passive = can_passive
+        v.can_feminin = can_feminin
+        v.reflexive_only = reflexive_only
+        v.can_reflexive = can_reflexive
+        v.is_second_form = is_second_form
+        v.is_frequent = is_frequent
+        v.is_transitive = is_transitive
+        v.is_intransitive = is_intransitive
+        v.is_pronominal = is_pronominal
+        v.aspirate_h = aspirate_h
+        v.belgium = belgium
+        v.africa = africa
+        v.conjugated_with_avoir = conjugated_with_avoir
+        v.conjugated_with_etre = conjugated_with_etre
+        v.is_defective = is_defective
+        v.is_impersonal = is_impersonal
+        v.book = book
+        v.is_rare = is_rare
+        v.is_archaique = is_archaique
+        v.is_slang = is_slang
+        v.group_no = group_no
+        v.regle_id = regle_id
+
+        v.save()
 
 
 class Command(BaseCommand):
