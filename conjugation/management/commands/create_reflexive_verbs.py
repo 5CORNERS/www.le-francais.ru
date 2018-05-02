359003b5c910f4d29fd2d502cb7e8ab611a741ee
AntonDumov <anton.dumov@gmail.com>
2018-05-02 21:33:28 +0300

    update tables commands

--- conjugation/management/commands/create_reflexive_verbs.py
+++ conjugation/management/commands/create_reflexive_verbs.py
@@ -6,8 +6,9 @@ from conjugation.models import Verb as V, ReflexiveVerb as RV
 class Command(BaseCommand):
     def handle(self, *args, **options):
         for v in V.objects.all():
-            if v.reflexive:
-                rv, created = RV.objects.get_or_create(infinitive=v.reflexive, verb=v)
+            if v.can_reflexive:
+                reflexive_infinitive = "se "+v.infinitive if v.infnitive_first_letter_is_vowel() else "s'"+v.infinitive
+                rv, created = RV.objects.get_or_create(infinitive=reflexive_infinitive, verb=v)
                 rv.create_no_accents()
                 rv.save()
                 print(rv.infinitive)
