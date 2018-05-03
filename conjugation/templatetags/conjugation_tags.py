461a35b49f9d038b4bed5b054adf3b04bca237fc
AntonDumov <anton.dumov@gmail.com>
2018-05-03 18:11:08 +0300

    ads and audio player added, sightbar rearrange

--- conjugation/templatetags/conjugation_tags.py
+++ conjugation/templatetags/conjugation_tags.py
@@ -1,5 +1,6 @@
 from django import template
 from django.urls import reverse
+from home.models import PageLayoutAdvertisementSnippet
 
 register = template.Library()
 
@@ -24,3 +25,17 @@ def option_url(context, option):
         pronominal = ''
 
     return reverse('conjugation:verb',kwargs=dict(feminin=gender, se=pronominal, verb=context.dicts[3]['v'].infinitive_no_accents))
+
+@register.inclusion_tag('tags/conjugation_advert_body.html')
+def conjugation_advertisement_body(page_type, placement):
+    try:
+        return dict(body=PageLayoutAdvertisementSnippet.objects.filter(page_type=page_type, placement=placement).exclude(live=False)[0].body)
+    except:
+        return dict(body=None)
+
+@register.inclusion_tag('tags/conjugation_advert_head.html')
+def conjugation_advertisement_head(page_type, placement):
+    try:
+        return dict(head=PageLayoutAdvertisementSnippet.objects.filter(page_type=page_type, placement=placement).exclude(live=False)[0].head)
+    except:
+        return dict(head=None)
