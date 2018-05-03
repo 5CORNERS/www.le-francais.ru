ba49cac83efb920b6214fdfc25bb6e88b4b6ddf5
AntonDumov <anton.dumov@gmail.com>
2018-05-03 12:08:14 +0300

    reflexive verbs creater fixed

--- conjugation/management/commands/create_reflexive_verbs.py
+++ conjugation/management/commands/create_reflexive_verbs.py
@@ -6,7 +6,7 @@ from conjugation.models import Verb as V, ReflexiveVerb as RV
 class Command(BaseCommand):
     def handle(self, *args, **options):
         for v in V.objects.all():
-            if v.can_reflexive:
+            if v.can_reflexive or v.reflexive_only:
                 reflexive_infinitive = "se "+v.infinitive if v.infnitive_first_letter_is_vowel() else "s'"+v.infinitive
                 rv, created = RV.objects.get_or_create(infinitive=reflexive_infinitive, verb=v)
                 rv.create_no_accents()
