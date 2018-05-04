83626b2ab74971cf0d8824574469435eda27da58
AntonDumov <anton.dumov@gmail.com>
2018-05-04 15:57:08 +0300

    advertisement tag,

--- conjugation/templatetags/conjugation_tags.py
+++ conjugation/templatetags/conjugation_tags.py
@@ -27,15 +27,15 @@ def option_url(context, option):
     return reverse('conjugation:verb',kwargs=dict(feminin=gender, se=pronominal, verb=context.dicts[3]['v'].infinitive_no_accents))
 
 @register.inclusion_tag('tags/conjugation_advert_body.html')
-def conjugation_advertisement_body(page_type, placement):
+def conjugation_advertisement_body(code):
     try:
-        return dict(body=PageLayoutAdvertisementSnippet.objects.filter(page_type=page_type, placement=placement).exclude(live=False)[0].body)
+        return dict(body=PageLayoutAdvertisementSnippet.objects.filter(code=code).exclude(live=False)[0].body)
     except:
         return dict(body=None)
 
 @register.inclusion_tag('tags/conjugation_advert_head.html')
-def conjugation_advertisement_head(page_type, placement):
+def conjugation_advertisement_head(code):
     try:
-        return dict(head=PageLayoutAdvertisementSnippet.objects.filter(page_type=page_type, placement=placement).exclude(live=False)[0].head)
+        return dict(head=PageLayoutAdvertisementSnippet.objects.filter(code=code).exclude(live=False)[0].head)
     except:
         return dict(head=None)
--- conjugation/urls.py
+++ conjugation/urls.py
@@ -6,7 +6,7 @@ urlpatterns = [
     url(r'verbs_autocomplete/.{0,50}?/$', views.get_autocomplete_list, name='autocomplete'),
     url(r'search/$', views.search, name='search'),
     url(r"(?P<feminin>feminin_)?(?P<se>se_)?(?P<verb>[-'a-zÀ-ÿ]{1,30})(?P<homonym>_2)?/$", views.verb, name='verb'),
-    url(r'$', views.index, name='index'),
+    url(r'^$', views.index, name='index'),
 ]
 
 
