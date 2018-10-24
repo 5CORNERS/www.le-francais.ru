176ec268c24be990af521b52bfb952a0e945ed14
Anton Dumov <anton.dumov@gmail.com>
2018-10-24 13:28:15 +0300

    fix notif urls

--- le_francais/urls.py
+++ le_francais/urls.py
@@ -80,8 +80,8 @@ urlpatterns = [
     url(r'^', include('social_django.urls'.format(extra), namespace='social')),
     url(r'', include('user_sessions.urls', 'user_sessions')),
     url(r'^new-users-redirect-url/', TemplateView.as_view(template_name='account/change_username_new.html')),
-    url(r'^', include(wagtail_urls)),
     url(r'^tinkoff/', include(tinkoff_urls)),
+    url(r'^', include(wagtail_urls)),
 
 ]
 
