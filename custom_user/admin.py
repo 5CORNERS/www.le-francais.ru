742b665dbc3772b945b4d2bb35f717af187a5346
Anton Dumov <anton.dumov@gmail.com>
2018-11-07 18:02:03 +0300

    custom_user/admin.py updated

--- custom_user/admin.py
+++ custom_user/admin.py
@@ -1,5 +1,19 @@
 from django.contrib import admin
 from django.contrib.auth.admin import UserAdmin
+from django.contrib.auth.forms import UserChangeForm
+
 from .models import User
 
-admin.site.register(User, UserAdmin)
+
+class CustomUserChangeForm(UserChangeForm):
+	class Meta(UserChangeForm.Meta):
+		model = User
+
+
+@admin.register(User)
+class CustomUserAdmin(UserAdmin):
+	form = CustomUserChangeForm
+
+	fieldsets = UserAdmin.fieldsets + (
+		(None, {'fields': ('cup_amount',)}),
+	)
