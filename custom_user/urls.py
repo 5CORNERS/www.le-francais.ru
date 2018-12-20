2e0895cdcc07850ee776dc788d542c674f56e4ce
Anton Dumov <anton.dumov@gmail.com>
2018-12-20 14:02:58 +0300

    some fixes

--- custom_user/urls.py
+++ custom_user/urls.py
@@ -1,6 +1,7 @@
 from django.conf.urls import url
-from .views import SawMessageView
+from .views import SawMessageView, AdminCommands
 
 urlpatterns = [
-	url('^/account/saw_message/$', SawMessageView.as_view(), name='saw_message')
+	url('^/account/saw_message/$', SawMessageView.as_view(), name='saw_message'),
+	url('admin_commands/$', AdminCommands.as_view(), name='admin_commands'),
 ]
