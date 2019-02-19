bdc44f7a0bcb436dbf36e5a6b01c54f67b7f3a4f
Anton Dumov <anton.dumov@gmail.com>
2019-02-19 17:28:51 +0300

    fixes

--- conjugation/management/commands/start_polly_tasks.py
+++ conjugation/management/commands/start_polly_tasks.py
@@ -7,18 +7,21 @@ from conjugation.polly import *
 
 class Command(BaseCommand):
 	def add_arguments(self, parser):
-		parser.add_argument('count', nargs='?', type=int, default=1)
+		parser.add_argument('count', nargs='?', type=int, default=2)
 
 	def handle(self, *args, **options):
 		api = PollyAPI()
 		verbs = list(Verb.objects.prefetch_related('reflexiveverb').all().order_by('-count'))[:options['count']]
 		tasks = []
-		for counter, v in enumerate(verbs):  # type: Verb
-			print('{0} in {1} {2}'.format(counter, options['count'], v))
+		print()
+		for counter, v in enumerate(verbs):  # type: int, Verb
+			print('\rCreating ssml texts... {0} in {1}'.format(counter+1, options['count']), end='')
 			for verb, gender, is_reflexive in v.get_all():
 				table = Table(verb, gender, is_reflexive)  # type: Table
 				for mood in table.moods:  # type: Mood
 					for tense in mood.tenses:  # type: Tense
+						if tense.is_empty():
+							continue
 						polly_audio, created = PollyAudio.objects.get_or_create(key=tense.key)
 						new_text = tense.get_polly_ssml()
 						if created or polly_audio.url is None or polly_audio.text != new_text:
@@ -29,6 +32,6 @@ class Command(BaseCommand):
 							polly_audio.voice_id = VOICE_ID_LEA
 							polly_audio.output_format = OUTPUT_FORMAT_MP3
 							tasks.append(polly_audio)
-		print('\nStart creating polly tasks')
+		print()
 		api.bulk_start_task(tasks)
-		print('\nDone!')
+		print('Done!')
