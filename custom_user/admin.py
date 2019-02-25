1b90dfdd68bc58a4154a6ad9d580b563333a0fcf
Anton Dumov <anton.dumov@gmail.com>
2019-02-25 15:40:20 +0300

    update custom_user

--- custom_user/admin.py
+++ custom_user/admin.py
@@ -5,6 +5,8 @@ from .models import User
 
 @admin.register(User)
 class CustomUserAdmin(admin.ModelAdmin):
-	list_display = ['username', 'email', 'date_joined', 'must_pay', 'saw_message', '_cup_amount', '_cup_credit', '_low_price', 'is_active']
+	exclude = ['password']
+	readonly_fields = ['username', 'email']
+	list_display = ['date_joined', 'must_pay', 'saw_message', '_cup_amount', '_cup_credit', '_low_price', 'is_active']
 	list_filter = ['date_joined']
 	search_fields = ['username', 'email', 'first_name', 'last_name']
