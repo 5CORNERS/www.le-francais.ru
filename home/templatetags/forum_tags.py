3a0bc4ab05840f6dfaa106b5bc11d5b7ee146c3b
Anton Dumov <anton.dumov@gmail.com>
2019-04-02 12:25:36 +0300

    reformat

--- home/templatetags/forum_tags.py
+++ home/templatetags/forum_tags.py
@@ -6,17 +6,19 @@ register = template.Library()
 
 @register.inclusion_tag('tags/entries.html')
 def last_topics(count=50, *args, **kwargs):
-    qs = Topic.objects.filter(forum__hidden=False).filter(forum__category__hidden=False)
-    qs = qs.filter(on_moderation=False)
-    qs = qs.order_by('-updated','-created')
-    qs = qs[:count]
-    return dict(object_list=qs, **{'to_end':True})
+	qs = Topic.objects.filter(forum__hidden=False).filter(
+		forum__category__hidden=False)
+	qs = qs.filter(on_moderation=False)
+	qs = qs.order_by('-updated', '-created')
+	qs = qs[:count]
+	return dict(object_list=qs, **{'to_end': True})
 
 
 @register.inclusion_tag('tags/entries.html')
 def top_topics(count=50, *args, **kwargs):
-    qs = Topic.objects.filter(forum__hidden=False).filter(forum__category__hidden=False)
-    qs = qs.filter(on_moderation=False)
-    qs = qs.order_by('-post_count')
-    qs = qs[:count]
-    return dict(object_list=qs, **{'to_end':False})
+	qs = Topic.objects.filter(forum__hidden=False).filter(
+		forum__category__hidden=False)
+	qs = qs.filter(on_moderation=False)
+	qs = qs.order_by('-post_count')
+	qs = qs[:count]
+	return dict(object_list=qs, **{'to_end': False})
