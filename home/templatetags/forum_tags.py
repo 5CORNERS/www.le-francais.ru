ed41037ea62abe53fb8329d3f01c413af050f4c0
Anton Dumov <anton.dumov@gmail.com>
2019-04-18 18:09:28 +0300

    change last_updated topics tag to ignore updated posts

--- home/templatetags/forum_tags.py
+++ home/templatetags/forum_tags.py
@@ -1,4 +1,6 @@
 from django import template
+from django.db.models import Max
+
 from pybb.models import Topic
 
 register = template.Library()
@@ -7,9 +9,9 @@ register = template.Library()
 @register.inclusion_tag('tags/entries.html')
 def last_topics(count=50, *args, **kwargs):
 	qs = Topic.objects.filter(forum__hidden=False).filter(
-		forum__category__hidden=False)
+		forum__category__hidden=False).prefetch_related('posts').annotate(last_post_created=Max('posts__created'))
 	qs = qs.filter(on_moderation=False)
-	qs = qs.order_by('-updated', '-created')
+	qs = qs.order_by('-last_post_created')
 	qs = qs[:count]
 	return dict(object_list=qs, **{'to_end': True})
 
