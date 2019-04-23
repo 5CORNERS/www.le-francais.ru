7c8bbbbeb5dbe6031576c28e54e6209829966d3e
Anton Dumov <anton.dumov@gmail.com>
2019-04-23 15:33:24 +0300

    in house ads placement

--- home/templatetags/site_tags.py
+++ home/templatetags/site_tags.py
@@ -30,6 +30,8 @@ def include_advertisements(contains=None, page_type=None, placement=None):
 		snippets = snippets.filter(page_type=page_type)
 	if placement:
 		snippets = snippets.filter(placement=placement)
+	snippets = list(snippets)
+	snippets.insert(1, AdUnit.objects.get(placement='in_house_sidebar'))
 	return {
 		'id': random_id, 'ads': snippets,
 		'mappings': list(set(ad.size_mapping for ad in snippets)),
