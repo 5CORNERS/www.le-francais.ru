aaf2adcd00535f85a84948187705de5bf1402d02
Anton Dumov <anton.dumov@gmail.com>
2019-05-28 17:33:16 +0300

    sitemap code updated

--- conjugation/sitemap.py
+++ conjugation/sitemap.py
@@ -5,5 +5,5 @@ from itertools import chain
 class ConjugationSitemap(Sitemap):
     def items(self):
         verb_queryset = Verb.objects.all()
-        reflexive_verb_queryset = ReflexiveVerb.objects.all()
+        reflexive_verb_queryset = ReflexiveVerb.objects.all().select_related('verb')
         return list(chain(verb_queryset, reflexive_verb_queryset))
