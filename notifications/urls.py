4cc7978537459810ef669abce4bffa60a4118e9e
Anton Dumov <anton.dumov@gmail.com>
2019-06-21 18:32:48 +0300

    real-time notifications

--- notifications/urls.py
+++ notifications/urls.py
@@ -1,8 +1,11 @@
 from django.conf.urls import url
 from .views import *
 
-notifications_api = [
+urlpatterns = [
     url(r'^get_notifications/$', get_notifications, name='get'),
+    url(r'^get_new_notifications/$', get_new_notifications, name='get_new'),
+    url(r'^get_new_notifications_count/$', get_new_notifications_count, name='get_new_count'),
+    url(r'^get_drop_content_html/$', get_drop_content_html, name='get_drop_content_html'),
     url(r'^check_notification/(?P<pk>\d+)/$', check_notification, name='check_pk'),
-    url(r'^check_notifications/', check_notifications, name='check_list')
+    url(r'^check_notifications/$', check_notifications, name='check_list'),
 ]
