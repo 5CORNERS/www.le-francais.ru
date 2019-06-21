dda11691aadc16ef1924d16372662ba76f1cd191
Anton Dumov <anton.dumov@gmail.com>
2019-06-21 18:32:48 +0300

    real-time notifications

--- notifications/static/notifications/js/notifications.js
+++ notifications/static/notifications/js/notifications.js
@@ -1,16 +1,50 @@
 $(document).ready(function () {
-	$('.dropdown-menu .notify').click(function (e) {
-		if (!$(e.target).is('a')) {
-			window.location.href = this.getAttribute('data-url')
+	$('.notify-dropdown').on('click', get_drop_content);
+	setInterval(update_badge, 10000);
+});
+
+function check_notifications(pks) {
+	if (pks.length > 0) {
+		$('#notify-badge-nav').hide().html('0');
+		$.ajax(Urls['notifications:check_list'](), {
+			type: 'POST',
+			data: {'pks': pks.toString()}
+		})
+	}
+}
+
+function update_badge() {
+	$.ajax({
+		url: Urls['notifications:get_new_count'](),
+		success: function (r) {
+			if (r > 0) {
+				$('#notify-badge-nav').html(r).show();
+			}else{
+				$('#notify-badge-nav').html(r).hide();
+			}
 		}
 	});
-	$('.notify-dropdown').one('click', function (e) {
-		if (NEW_NOTIFY_PKS.length > 0) {
-			$('.notify-badge').hide();
-			$.ajax(CHECK_URL, {
-				type: 'POST',
-				data: {'pks': NEW_NOTIFY_PKS.toString()}
-			})
+}
+
+function get_drop_content() {
+	$('.notify-drop > .drop-content').replaceWith($("<div class=\"drop-content\"><img src='static/images/loading_icon.gif'></div>"));
+	$.ajax(Urls['notifications:get_drop_content_html'](), {
+		data: {
+			path: window.location.pathname,
+		},
+		success: function (r) {
+			$('.notify-drop > .drop-content').replaceWith(r);
+			$('.dropdown-menu .notify').click(function (e) {
+				if (!$(e.target).is('a')) {
+					window.location.href = this.getAttribute('data-url')
+				}
+			});
+			var new_notifications_pks = [];
+			$("*[data-check_datetime='None']").each(function () {
+				new_notifications_pks.push(this.dataset.pk)
+			});
+			check_notifications(new_notifications_pks)
 		}
 	})
-});
+}
+
