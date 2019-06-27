abc46f0c58680f519af0a86b781fdb750f7018ef
Anton Dumov <anton.dumov@gmail.com>
2019-06-27 16:27:23 +0300

    get log view

--- tinkoff_merchant/urls.py
+++ tinkoff_merchant/urls.py
@@ -1,6 +1,7 @@
 from django.conf.urls import url
-from .views import Notification
+from .views import Notification, get_log
 
 urlpatterns = [
     url('^notification/$', Notification.as_view(), name='notification'),
+    url('^log/([0-9]{4})/([0-9]{2})/$', get_log, name='get_log')
 ]
