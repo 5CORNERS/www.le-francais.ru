284822233585b18c6b801fd0057acead10ef4f09
Anton Dumov <anton.dumov@gmail.com>
2019-07-17 18:53:11 +0300

    update dictionary

--- le_francais_dictionary/admin.py
+++ le_francais_dictionary/admin.py
@@ -5,11 +5,12 @@ from django.conf.urls import url
 from django.contrib import admin
 from django.db import transaction
 from django.shortcuts import redirect, render
-from django.template.response import TemplateResponse
 
 from home.models import LessonPage
-from .models import Word, WordTranslation
 from .forms import DictionaryCsvImportForm
+from .models import Word, WordTranslation
+
+
 # Register your models here.
 
 def create_polly_task(modeladmin: admin.ModelAdmin, request, qs):
@@ -25,6 +26,10 @@ class WordTranslationInline(admin.TabularInline):
 @admin.register(Word)
 class WordAdmin(admin.ModelAdmin):
 	change_list_template = 'dictionary/admin/change_list.html'
+	ordering = ['cd_id']
+	list_display = ['word']
+	list_filter = ['genre']
+	list_select_related = []
 	readonly_fields = ['polly']
 	inlines = [WordTranslationInline]
 	actions = [create_polly_task]
@@ -44,22 +49,46 @@ class WordAdmin(admin.ModelAdmin):
 				return redirect('admin:le_francais_dictionary_word_changelist')
 			csv_file = TextIOWrapper(request.FILES['csv_file'].file, encoding='utf-8', errors='replace')
 			reader = csv.reader(csv_file)
-			with transaction.atomic():
-				for i, row in enumerate(reader):
-					print('\r' + str(int(i / 3641 * 100)) + '%', end='')
-					if row[0]:
-						word = Word.objects.create(
-							word=row[3],
-							cd_id=int(row[0].split('.')[0]),
-							genre=row[2] if row[2] == 'm' or row[2] == 'f' else None,
+			to_create = dict(
+				words=[],
+				translations=[],
+				lesson_relations=[],
+			)
+			lesson_ids = dict(LessonPage.objects.values_list('lesson_number', 'id'))
+			for i, row in enumerate(reader):
+				if row[0]:
+					print('\r' + str(
+						int(i / 3641 * 100)), end='')
+					word = Word(
+						id=i,
+						word=row[3],
+						cd_id=row[0],
+						genre=row[2] if row[2] in ['m','f'] else None,
 						)
-						WordTranslation.objects.create(
-							translation=row[4],
-							word=word,
-						)
-						if row[1]:
-							word.lessons.add(LessonPage.objects.get(lesson_number=row[1]))
-			print('\nDone!')
+					translation = WordTranslation(
+						word_id=i,
+						translation=row[4]
+					)
+					lesson_relation = Word.lessons.through(
+						word_id = i,
+						lessonpage_id = lesson_ids[int(row[1])]
+					) if row[1] else None
+					to_create['words'].append(word)
+					to_create['translations'].append(translation)
+					to_create['lesson_relations'].append(lesson_relation)
+			print('\nBulk creating words')
+			Word.objects.bulk_create(
+				[w for w in to_create['words']]
+			)
+			print('Bulk crating translations')
+			WordTranslation.objects.bulk_create(
+				[wt for wt in to_create['translations']]
+			)
+			print('Bulk creating relations')
+			Word.lessons.through.objects.bulk_create(
+				[wl for wl in filter(None.__ne__, to_create['lesson_relations'])]
+			)
+			print('Done!')
 			self.message_user(request, 'CSV file has been imported')
 			return redirect('admin:le_francais_dictionary_word_changelist')
 		form = DictionaryCsvImportForm()
--- le_francais_dictionary/views.py
+++ le_francais_dictionary/views.py
@@ -1,3 +1,17 @@
+import json
+
+from django.http import JsonResponse
 from django.shortcuts import render
 
 # Create your views here.
+from home.models import LessonPage
+from le_francais_dictionary.models import Word
+
+
+def get_words(request, lesson_number):
+	words = Word.objects.prefetch_related(
+		'wordtranslation_set',
+		'wordtranslation_set__polly',
+		'polly').order_by('cd_id').filter(lessons=LessonPage.objects.get(lesson_number=lesson_number))
+	words = [word.to_dict() for word in words]
+	return JsonResponse({'words': words}, safe=False)
