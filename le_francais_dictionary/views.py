7072aa1cc470651db6ede29226d11e2bf630a1fb
Anton Dumov <anton.dumov@gmail.com>
2019-08-09 12:54:53 +0300

    return only one lesson_related words

--- le_francais_dictionary/views.py
+++ le_francais_dictionary/views.py
@@ -13,7 +13,7 @@ def get_words(request, lesson_number):
 		'polly',
 		'lessons'
 	).order_by('pk').filter(
-		lessons__lesson_number__in=list(range(lesson_number+1))[-6:])
+		lessons__lesson_number=lesson_number)
 	words_dict = [word.to_dict() for word in words]
 	if request.user.is_authenticated:
 		user_words = WordUser.objects.filter(
@@ -26,21 +26,7 @@ def get_words(request, lesson_number):
 	return JsonResponse({'words': words_dict, 'user_words': user_words_dict}, safe=False)
 
 
-def get_words_alt(request, lesson_number):
-	lesson_number = int(lesson_number)
-	from django.core import serializers
-	words = Word.objects.prefetch_related(
-		'wordtranslation_set',
-		'wordtranslation_set__polly',
-		'polly',
-		'lessons'
-	).order_by('pk').filter(lessons__lesson_number__in=list(range(lesson_number+1))[-6:])
-	data = serializers.serialize('json', words)
-	return HttpResponse(data, content_type='application/json')
-
-
-def push_changes(request):
+def add_user_words(request):
 	user = request.user
 	words = Word.json_to_query(request.POST.get('words'))
-
-	Word.objects.bulk_update(words, exclude_fields=['pk'])
+	
