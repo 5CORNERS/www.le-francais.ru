826db2ddacc46f0b408b82d5bd85e5d4b80e54d4
Anton Dumov <anton.dumov@gmail.com>
2019-08-09 13:14:45 +0300

    fix

--- le_francais_dictionary/urls.py
+++ le_francais_dictionary/urls.py
@@ -1,7 +1,6 @@
 from django.conf.urls import url
-from .views import *
+from .views import get_words
 
 urlpatterns = [
 	url('words/([0-9]*)/$', get_words, name='get_words'),
-	url('words_alt/([0-9]*)/', get_words_alt, name='get_words_alt'),
 ]
