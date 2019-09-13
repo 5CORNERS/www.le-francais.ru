f1c148a84052ba6e11d82dc9d545e9c5bcb2ad81
Anton Dumov <anton.dumov@gmail.com>
2019-09-13 19:22:14 +0300

    update dictionary

--- le_francais_dictionary/views.py
+++ le_francais_dictionary/views.py
@@ -41,19 +41,23 @@ def add_packets(request):
 
 
 def get_progress(request):
-	user = request.user
-	user_packets = list(UserPacket.objects.select_related('packet').filter(user=user).annotate(
-		words_count=Count('packet__word'),
-	))
+	last_lesson_number = request.user.latest_lesson_number
+	packets:List[Packet] = Packet.objects.filter(
+		Q(lesson__lesson_number__lte=last_lesson_number if last_lesson_number is not None and last_lesson_number > 10 else 10)|Q(
+			userpacket__user=request.user
+		)
+	)
+	activated_lessons = list(request.user.payed_lessons.all().values_list('id', flat=True))
+	added_packets = Packet.objects.filter(userpacket__user=request.user).values_list('id', flat=True)
 	packets_data = []
-	for user_packet in user_packets:
+	for packet in packets:
 		packets_data.append(dict(
-			pk=user_packet.packet_id,
-			name=user_packet.packet.name,
-			activated=True,
-			added=True,
-			wordsCount=user_packet.words_count,
-			wordsLearned=UserWordData.objects.filter(grade=1, user=user, word__packet=user_packet.packet).count()
+			pk=packet.pk,
+			name=packet.name,
+			activated=True if packet.lesson_id in activated_lessons else False,
+			added=True if packet.pk in added_packets else False,
+			wordsCount=packet.words_count,
+			wordsLearned=Word.objects.filter(userdata__user=request.user, userdata__grade=1, packet=packet).count()
 		))
 	return JsonResponse({'packets': packets_data})
 
@@ -67,5 +71,21 @@ def get_words(request, packet_id):
 	words_dict = [word.to_dict() for word in words]
 	return JsonResponse({'words': words_dict}, safe=False)
 
-def update_user_words(request):
-	...
+def update_words(request):
+	words_data = json.loads(request.body)['words']
+	user_words:List[UserWordData] = []
+	for word in words_data:
+		user_words.append(UserWordData(
+			word_id=word['pk'],
+			user_id=request.user.id,
+			grade=word['grade'],
+			mistakes=word['mistakes'],
+		))
+	user_words = UserWordData.objects.bulk_create(user_words)
+	result = []
+	for user_word in user_words:
+		result.append(dict(
+			pk=user_word.word_id,
+			nextRepetition=user_word.get_next_repetition_datetime()
+		))
+	return JsonResponse(result, safe=False)
