4ccababc2f1c872c58464a07f1ba136d5f40b6ef
Anton Dumov <anton.dumov@gmail.com>
2019-09-23 22:08:12 +0300

    add cups admin action

--- custom_user/forms.py
+++ custom_user/forms.py
@@ -2,10 +2,14 @@ from django import forms
 from snowpenguin.django.recaptcha3.fields import ReCaptchaField
 from snowpenguin.django.recaptcha3.widgets import ReCaptchaHiddenInput
 
+
 class CaptchaAllauthSignupForm(forms.Form):
+	captcha = ReCaptchaField(widget=ReCaptchaHiddenInput, score_threshold=0.1)
 
-    captcha = ReCaptchaField(widget=ReCaptchaHiddenInput, score_threshold=0.1)
+	def signup(self, request, user):
+		user.recaptcha3_score = self.cleaned_data['captcha'][1]
+		user.save()
 
-    def signup(self, request, user):
-        user.recaptcha3_score = self.cleaned_data['captcha'][1]
-        user.save()
+
+class UserAddCupsForm(forms.Form):
+	cups = forms.IntegerField()
