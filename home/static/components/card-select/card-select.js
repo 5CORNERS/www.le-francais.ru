591f3e772c097264e5972f527670052168156817
Anton Dumov <anton.dumov@gmail.com>
2019-10-17 17:10:42 +0300

    remove unnecessary files

--- home/static/components/card-select/card-select.js
+++
@@ -1,6 +0,0 @@
-$('[data-cardSelectButton]').click(function() {
-  $('.is-selected').each(function (i,e) {
-    $(e).toggleClass('is-selected')
-  });
-  $(this).parent('[data-cardSelect]').toggleClass('is-selected');
-});
