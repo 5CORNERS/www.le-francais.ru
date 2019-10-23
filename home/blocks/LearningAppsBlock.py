969ca1d5eac5ebb55f21ee3956a7c54f9454374c
Anton Dumov <anton.dumov@gmail.com>
2019-10-23 20:03:06 +0300

    update management command, added data files, other fixes

--- home/blocks/LearningAppsBlock.py
+++ home/blocks/LearningAppsBlock.py
@@ -1,15 +1,15 @@
 from wagtail.core import blocks
 from wagtail.core.blocks.field_block import CharBlock, IntegerBlock, \
-    BooleanBlock
+    BooleanBlock, RawHTMLBlock
 
 
 class LearningAppsBlock(blocks.StructBlock):
-    app_id = CharBlock()
-    number = IntegerBlock()
-    show_lesson_number = BooleanBlock()
-    title = CharBlock()
-    height = IntegerBlock()
-    width = IntegerBlock()
+    app_id = CharBlock(required=True)
+    number = IntegerBlock(required=True)
+    show_lesson_number = BooleanBlock(default=True)
+    title = CharBlock(required=False)
+    height = CharBlock(default='500px')
+    width = CharBlock(default='100%')
 
     class Meta:
         template = 'blocks/learning_apps_block.html'
