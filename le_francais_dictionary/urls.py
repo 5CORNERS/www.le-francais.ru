e9c49135e57f4e5c0be77cc98cde209983f45e28
Anton Dumov <anton.dumov@gmail.com>
2019-10-26 13:00:16 +0300

    clear_all method added

--- le_francais_dictionary/urls.py
+++ le_francais_dictionary/urls.py
@@ -1,12 +1,13 @@
 from django.conf.urls import url
 
 from le_francais_dictionary.views import get_repetition_words
-from .views import get_words, get_progress, add_packets, update_words
+from .views import get_words, get_progress, add_packets, update_words, clear_all
 
 urlpatterns = [
 	url('words/([0-9]*)/$', get_words, name='get_words'),
 	url('repetitions/$', get_repetition_words, name='get_repetitions'),
 	url('update-words/$', update_words, name='update_words'),
 	url('progress/$', get_progress, name='get_progress'),
-	url('add-packets/$', add_packets, name='add_packets')
+	url('add-packets/$', add_packets, name='add_packets'),
+	url('clear_all/$', clear_all, name='clear_all'),
 ]
