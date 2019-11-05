f1f0b07f16e438d942655f91a7506bf1e6d0a2c9
Anton Dumov <anton.dumov@gmail.com>
2019-11-05 19:41:10 +0300

    added packet id to html template

--- le_francais_dictionary/templatetags/le_francais_dictionary.py
+++ le_francais_dictionary/templatetags/le_francais_dictionary.py
@@ -4,5 +4,20 @@ register = template.Library()
 
 
 @register.inclusion_tag('dictionary/dictionary.html')
-def include_dictionary():
-    return
+def include_dictionary(lesson_page=None):
+    """
+    :type lesson_page: home.models.LessonPage
+    """
+    if lesson_page:
+        return {'lesson_page': lesson_page}
+
+@register.assignment_tag()
+def get_packet_id(lesson_page):
+    """
+    :type lesson_page: home.models.LessonPage
+    """
+    try:
+        packet = lesson_page.dictionary_packets.order_by('id').first()
+    except:
+        return None
+    return packet.id
