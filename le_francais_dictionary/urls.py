54b51b2a25f3bee9437ab0d592262138ed0b8e71
Anton Dumov <anton.dumov@gmail.com>
2019-11-11 16:31:31 +0300

    url full match

--- le_francais_dictionary/urls.py
+++ le_francais_dictionary/urls.py
@@ -3,13 +3,13 @@ from django.conf.urls import url
 from le_francais_dictionary.views import get_repetition_words
 from . import views
 urlpatterns = [
-    url('words/([0-9]*)/$', views.get_words, name='get_words'),
-    url('repeat-words/$', views.get_repetition_words, name='get_repetitions'),
-    url('update-words/$', views.update_words, name='update_words'),
-    url('progress/$', views.get_progress, name='get_progress'),
-    url('progress/(?P<pk>\d+)/$', views.get_packet_progress, name='get_packet_progress'),
-    url('add-packets/$', views.add_packets, name='add_packets'),
-    url('clear_all/$', views.clear_all, name='clear_all'),
-    url('mark-words/$', views.mark_words, name='mark_words'),
+    url('^words/([0-9]*)/$', views.get_words, name='get_words'),
+    url('^repeat-words/$', views.get_repetition_words, name='get_repetitions'),
+    url('^update-words/$', views.update_words, name='update_words'),
+    url('^progress/$', views.get_progress, name='get_progress'),
+    url('^progress/(?P<pk>\d+)/$', views.get_packet_progress, name='get_packet_progress'),
+    url('^add-packets/$', views.add_packets, name='add_packets'),
+    url('^clear_all/$', views.clear_all, name='clear_all'),
+    url('^mark-words/$', views.mark_words, name='mark_words'),
 ]
 
