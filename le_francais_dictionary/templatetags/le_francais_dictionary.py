6ed50d6ef038015ca2f98662afdda83d3ad28f22
Anton Dumov <anton.dumov@gmail.com>
2019-11-20 18:15:05 +0300

    update tags

--- le_francais_dictionary/templatetags/le_francais_dictionary.py
+++ le_francais_dictionary/templatetags/le_francais_dictionary.py
@@ -1,15 +1,19 @@
 from django import template
 
+from le_francais_dictionary.models import UserPacket
+
 register = template.Library()
 
 
 @register.inclusion_tag('dictionary/dictionary_tab.html', takes_context=True)
-def include_dictionary(context, lesson_page=None):
+def include_dictionary(context, lesson_page):
     """
     :type lesson_page: home.models.LessonPage
     """
-    if lesson_page:
-        return {'lesson_page': lesson_page, 'hide_info': context['request'].COOKIES.get('hide_flash_cards_info', None)}
+    return {'lesson_page': lesson_page,
+            'hide_info': context['request'].COOKIES.get(
+                'hide_flash_cards_info', None),
+            'user': context['request'].user}
 
 @register.assignment_tag()
 def get_packet_id(lesson_page):
@@ -22,12 +26,16 @@ def get_packet_id(lesson_page):
         return None
     return packet.id
 
-@register.assignment_tag()
-def get_packets(lesson_page):
+@register.assignment_tag(takes_context=True)
+def get_packets(context, lesson_page):
     """
     :type lesson_page: home.models.LessonPage
     """
     packets = lesson_page.get_word_packets()
+    # FIXME: remove adding packets
+    if context['request'].user.is_authenticated:
+        for packet in packets:
+            userpacker, created = UserPacket.objects.get_or_create(packet=packet, user=context['request'].user)
     if packets.__len__() > 1:
         return packets
     elif not packets:
