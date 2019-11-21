ddb9b5631fe6de69462d36df641b7bb153efe530
Anton Dumov <anton.dumov@gmail.com>
2019-11-21 21:03:51 +0300

    fix dictionaries
    it worked!
    also updated django and whitenoise

--- le_francais/wsgi.py
+++ le_francais/wsgi.py
@@ -12,9 +12,7 @@ from __future__ import absolute_import, unicode_literals
 import os
 
 from django.core.wsgi import get_wsgi_application
-from whitenoise.django import DjangoWhiteNoise
 
 os.environ.setdefault("DJANGO_SETTINGS_MODULE", "le_francais.settings.production")
 
 application = get_wsgi_application()
-application = DjangoWhiteNoise(application)
