3928f1240857b717c9d633a641c2d511a55a7f18
Anton Dumov <anton.dumov@gmail.com>
2019-11-29 22:05:13 +0300

    update

--- le_francais_dictionary/static/dictionary/js/user-table.js
+++ le_francais_dictionary/static/dictionary/js/user-table.js
@@ -5,9 +5,8 @@ var style = getComputedStyle(document.body);
 var checked_ids = [];
 
 (function($, F){
-
     // Extend the Row.$create method to add an id attribute to each <tr>.
-    F.Row.extend("$create", function(){
+    F.Row.extend("$create", function () {
         // call the original method
         this._super();
         // get the current row values
@@ -17,6 +16,61 @@ var checked_ids = [];
     });
 
 })(jQuery, FooTable);
+
+FooTable.MyFiltering = FooTable.Filtering.extend({
+        construct: function (instance) {
+            this._super(instance);
+
+            this.statuses = ['Active', 'Disabled', 'Suspended']; // the options available in the dropdown
+            this.def = 'Any Status'; // the default/unselected value for the dropdown (this would clear the filter when selected)
+            this.$status = null; // a placeholder for our jQuery wrapper around the dropdown
+        },
+        $create: function() {
+            this._super(); // call the base $create method, this populates the $form property
+            var self = this, // hold a reference to my self for use later
+                // create the bootstrap form group and append it to the form
+                $form_grp = $('<div/>', {'class': 'form-group'})
+                    .append($('<label/>', {
+                        'class': 'sr-only',
+                        text: 'Status'
+                    }))
+                    .prependTo(self.$form);
+
+            // create the select element with the default value and append it to the form group
+            self.$status = $('<select/>', {'class': 'form-control'})
+                .on('change', {self: self}, self._onStatusDropdownChanged)
+                .append($('<option/>', {text: self.def}))
+                .appendTo($form_grp);
+
+            // add each of the statuses to the dropdown element
+            $.each(self.statuses, function (i, status) {
+                self.$status.append($('<option/>').text(status));
+            });
+        },
+        _onStatusDropdownChanged: function (e) {
+            var self = e.data.self, // get the MyFiltering object
+                selected = $(this).val(); // get the current dropdown value
+            if (selected !== self.def) { // if it's not the default value add a new filter
+                self.addFilter('difficulty', selected, ['difficulty']);
+            } else { // otherwise remove the filter
+                self.removeFilter('difficulty');
+            }
+            // initiate the actual filter operation
+            self.filter();
+        },
+        draw: function () {
+            this._super(); // call the base draw method, this will handle the default search input
+            var status = this.find('difficulty'); // find the status filter
+            if (status instanceof FooTable.Filter) { // if it exists update the dropdown to reflect the value
+                this.$status.val(status.query.val());
+            } else { // otherwise update the dropdown to the default value
+                this.$status.val(this.def);
+            }
+        }
+
+    });
+
+FooTable.components.register('filtering', FooTable.MyFiltering);
 
 function getStars(rating) {
 
