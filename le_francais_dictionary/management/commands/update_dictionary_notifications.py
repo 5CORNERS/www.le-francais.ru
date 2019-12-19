565a85b1e8c68a8e16bf7b38b7ed714d6ab76221
Anton Dumov <anton.dumov@gmail.com>
2019-12-19 16:56:45 +0300

    enable update to all users

--- le_francais_dictionary/management/commands/update_dictionary_notifications.py
+++ le_francais_dictionary/management/commands/update_dictionary_notifications.py
@@ -6,7 +6,7 @@ from notifications.models import Notification, NotificationUser
 
 
 def update_notifications():
-	notifications = Notification.objects.filter(category=Notification.INTERVAL_REPETITIONS, notificationuser__user_id=763)
+	notifications = Notification.objects.filter(category=Notification.INTERVAL_REPETITIONS)
 	for notification in notifications:
 		user_notification = notification.notificationuser_set.first()
 		day_repetitions = UserDayRepetition.objects.filter(repetitions__contains=[notification.object_id], user=user_notification.user)
@@ -14,6 +14,7 @@ def update_notifications():
 			notification.content_object = day_repetitions.first()
 			notification.data['qty'] = len(day_repetitions.first().repetitions)
 			notification.save()
+			print(day_repetitions)
 
 
 class Command(BaseCommand):
