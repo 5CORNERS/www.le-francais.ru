1318f0389dd3e69517c5f7b64933cbf7978930f3
Anton Dumov <anton.dumov@gmail.com>
2019-12-19 17:30:41 +0300

    qty to str

--- le_francais_dictionary/management/commands/update_dictionary_notifications.py
+++ le_francais_dictionary/management/commands/update_dictionary_notifications.py
@@ -4,6 +4,15 @@ from django.core.management import BaseCommand
 from le_francais_dictionary.models import UserDayRepetition
 from notifications.models import Notification, NotificationUser
 
+def message(n, form1=' новое слово', form2=' новых слова', form5=' новых слов'):
+    n10 = n % 10
+    n100 = n % 100
+    if n10 == 1 and n100 != 11:
+        return '{0} {1}'.format(str(n), form1)
+    elif n10 in [2, 3, 4] and n100 not in [12, 13, 14]:
+        return '{0} {1}'.format(str(n), form2)
+    else:
+        return '{0} {1}'.format(str(n), form5)
 
 def update_notifications():
 	notifications = Notification.objects.filter(category=Notification.INTERVAL_REPETITIONS)
@@ -12,7 +21,8 @@ def update_notifications():
 		day_repetitions = UserDayRepetition.objects.filter(repetitions__contains=[notification.object_id], user=user_notification.user)
 		if day_repetitions:
 			notification.content_object = day_repetitions.first()
-			notification.data['qty'] = len(day_repetitions.first().repetitions)
+			quantity = len(day_repetitions.first().repetitions)
+			notification.data['qty'] = message(quantity)
 			notification.save()
 			print(day_repetitions)
 
