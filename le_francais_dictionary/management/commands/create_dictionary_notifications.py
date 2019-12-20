54c6776f817dfd1968d3fef9bed88183687af8a1
Anton Dumov <anton.dumov@gmail.com>
2019-12-20 17:30:22 +0300

    add sending failed notifications function

--- le_francais_dictionary/management/commands/create_dictionary_notifications.py
+++ le_francais_dictionary/management/commands/create_dictionary_notifications.py
@@ -1,12 +1,13 @@
 from django.core.management import BaseCommand
 from django.utils import timezone
 from notifications.models import create_dictionary_notification
-from le_francais_dictionary.models import UserDayRepetition
+from le_francais_dictionary.models import UserDayRepetition, UserWordRepetition
 
 
 class Command(BaseCommand):
 	def handle(self, *args, **options):
 		create_notifications()
+		create_failed_notifications()
 
 
 def create_notifications():
@@ -21,3 +22,18 @@ def create_notifications():
 			r.success = False
 			r.save()
 			print(f'Error creating notification {r.pk}')
+
+
+def create_failed_notifications():
+	deferred_repetitions = UserDayRepetition.objects.filter(
+		success=False
+	)
+	for deferred_repetition in deferred_repetitions:
+		for i, r_id in reversed(list(enumerate(deferred_repetition.repetitions))):
+			word_repetition = UserWordRepetition.objects.get(pk=r_id)
+			if word_repetition.repetition_datetime != deferred_repetition.datetime:
+				deferred_repetition.repetitions.pop(i)
+		if deferred_repetition.repetitions:
+			create_dictionary_notification(UserDayRepetition, deferred_repetition)
+		deferred_repetition.success = True
+		deferred_repetition.save()
