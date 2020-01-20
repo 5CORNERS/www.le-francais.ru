3101a2e298becf7a4223c6eeb6867ebabd229fd6
Anton Dumov <anton.dumov@gmail.com>
2020-01-20 18:20:25 +0300

    exclude false

--- le_francais_dictionary/management/commands/create_dictionary_notifications.py
+++ le_francais_dictionary/management/commands/create_dictionary_notifications.py
@@ -11,7 +11,12 @@ class Command(BaseCommand):
 
 
 def create_notifications():
-	now_repetitions = UserDayRepetition.objects.filter(datetime__lte=timezone.now()).exclude(repetitions__len=0, success=True)
+	now_repetitions = UserDayRepetition.objects.filter(
+		datetime__lte=timezone.now(),
+		success__isnull=True
+	).exclude(
+		repetitions__len=0
+	)
 	for r in now_repetitions:
 		try:
 			create_dictionary_notification(UserDayRepetition, r)
