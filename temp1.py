ece11f1431e34a23f034a58a055ae610d26c0597
Anton Dumov <anton.dumov@gmail.com>
2020-01-20 14:36:29 +0300

    update tests

--- temp1.py
+++ temp1.py
@@ -1,18 +1,20 @@
-to_rename = [(1185,"un_arret_de_bus_synth_hom.mp3"),
-(1360,"une_cause_synth_hom.mp3"),
-(1519,"le_ski_alpin_synth_hom.mp3"),
-(1520,"le_ski_de_fond_synth_hom.mp3"),
-(1521,"le_fond_synth_hom.mp3"),
-(1522,"la_luge_synth_hom.mp3"),
-(1523,"le_surf_des_neiges_synth_hom.mp3"),
-(1535,"les_oeufs_de_poisson_synth_hom.mp3"),
-(47,"a_synth.mp3"),]
-
+import csv
+import os
+from shutil import copyfile
 from le_francais_dictionary.models import Word
 
-for pk, new_filename in to_rename:
-	word = Word.objects.get(pk=pk)
-	old_url = word._polly_url
-	word._polly_url = old_url.rsplit('/', 1)[0] + '/' + new_filename
-	word.save()
-	print(word, old_url, '=>', word._polly_url)
+def update_base():
+	dir = 'D:/Sound/SoundF3/'
+	with open('le_francais_dictionary/local/base_update.csv', encoding='utf-8') as csv_file:
+		csv_reader = csv.DictReader(csv_file)
+		for row in csv_reader:
+			word = Word.objects.get(pk=row['IDX'])
+			if not os.path.exists(dir + row['Filename']):
+				print(row['IDX'], row['Filename'], "Can't find", sep=' ---- ')
+			else:
+				print(row['IDX'], row['Filename'], sep=' ---- ')
+				word.word = row['Title']
+				old_url = word._polly_url
+				word._polly_url = old_url.rsplit('/', 1)[0] + '/' + row['Filename']
+				print(old_url, '=>', word._polly_url)
+				word.save()
