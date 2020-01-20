014ec4fa005be4f8b310454822fcd859d9c042d8
Anton Dumov <anton.dumov@gmail.com>
2020-01-20 17:58:19 +0300

    Update remove_marked_words_from_dayrepetitions.py

--- le_francais_dictionary/management/commands/remove_marked_words_from_dayrepetitions.py
+++ le_francais_dictionary/management/commands/remove_marked_words_from_dayrepetitions.py
@@ -1,4 +1,5 @@
 from django.core.management import BaseCommand
+from django.utils import timezone
 
 from le_francais_dictionary.models import UserDayRepetition, \
 	UserWordRepetition, UserWordIgnore
@@ -6,10 +7,11 @@ from le_francais_dictionary.models import UserDayRepetition, \
 
 class Command(BaseCommand):
 	def handle(self, *args, **options):
-		for day_repetition in UserDayRepetition.objects.all():
+		for day_repetition in UserDayRepetition.objects.filter(datetime__gte=timezone.now()):
 			for pk in reversed(day_repetition.repetitions):
 				repetition = UserWordRepetition.objects.get(pk=pk)
 				if UserWordIgnore.objects.filter(user=day_repetition.user, word=repetition.word).exists():
 					print(f'{day_repetition.user} -- {repetition.word}')
 					day_repetition.repetitions.remove(pk)
+					day_repetition.save()
 					print(f'{pk} <= {day_repetition.repetitions}')
