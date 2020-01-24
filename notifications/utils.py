0349056f03ca5a775f0a1c6c9e675fa10372fb16
Anton Dumov <anton.dumov@gmail.com>
2020-01-24 21:55:12 +0300

    refactor, prefetch and visited attribute added

--- notifications/utils.py
+++ notifications/utils.py
@@ -29,43 +29,43 @@ def blank_list(request):
 
 def query_notifications(request):
 	login_url = settings.LOGIN_URL + '?next=' + request.GET.get('path', '')
-	if request.user.is_anonymous:
+	user = request.user
+	if user.is_anonymous:
 		return dict(authenticated=False, login_url=login_url)
 	notifications: List[Notification] = list(
-		Notification.objects.prefetch_related('notificationuser_set',
-		                                      'image').filter(
-			Q(to_all=True) | Q(notificationuser__user=request.user),
+		Notification.objects.select_related('image').filter(
+			Q(to_all=True) | Q(notificationuser__user=user),
 			active=True
-		).exclude(datetime_creation__lt=request.user.date_joined).exclude(
-			excpt=request.user).distinct().order_by(
+		).exclude(datetime_creation__lt=user.date_joined).exclude(
+			excpt=user).distinct().order_by(
 			'-datetime_creation'))  # TODO: фильтровать по разрешениям
 	if not notifications:
 		has_notifications = False
 	else:
 		has_notifications = True
-	new_notifications: List[Notification] = []
-	for notify in notifications:  # type: Notification
-		try:
-			notification_user = notify.notificationuser_set.get(
-				user=request.user)
-			if notification_user.check_datetime is None:
-				new_notifications.append(notify)
-		except NotificationUser.DoesNotExist:
-			if notify.to_all:
-				new_notifications.append(notify)
-		except NotificationUser.MultipleObjectsReturned:
-			first_notify = notify.notificationuser_set.filter(
-				user=request.user)[:1].values_list("id", flat=True)
-			notify.notificationuser_set.exclude(
-				pk__in=list(first_notify)).delete()
-			query_notifications(request)
+	notification_user_set = NotificationUser.objects.filter(
+		user=user
+	)
+	notification_user_dict = {n.notification_id:n for n in notification_user_set}
+	for notification in notifications:  # type: Notification
+		notification_user = notification_user_dict.get(notification.pk, None)
+		if notification_user is not None:
+			if notification_user.is_viewed():
+				notification._is_viewed[user.pk] = True
+			if notification_user.is_visited():
+				notification._is_visited[user.pk] = True
+		else:
+			notification._is_viewed[user.pk] = False
+			notification._is_visited[user.pk] = False
 	time_threshold = timezone.now() - timezone.timedelta(days=14)
-	old_notifications: List[Notification] = [x for x in notifications if
-	                                         x not in new_notifications and x.datetime_creation > time_threshold]
 	return dict(
-		authenticated=True,
 		has_notifications=has_notifications,
-		new_notifications=new_notifications,
-		old_notifications=old_notifications,
+		authenticated=True,
+		old_notifications=[
+			n.to_dict(user) for n in notifications if
+			(n.datetime_creation > time_threshold or not n.is_viewed(user))],
+		new_notifications=[
+			n.to_dict(user) for n in notifications if not n.is_viewed(user)
+		],
 		login_url=login_url,
 	)
