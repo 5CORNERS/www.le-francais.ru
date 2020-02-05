7c37d3baca80748268532796746522c26e89bbf3
Anton Dumov <anton.dumov@gmail.com>
2020-02-05 19:43:22 +0300

    search verb by form

--- conjugation/utils.py
+++ conjugation/utils.py
@@ -1435,3 +1435,15 @@ AUDIO_LIST = [
     "guider",
     "piquer",
     ]
+
+import collections
+
+def flatten(d, parent_key='', sep='_'):
+    items = []
+    for k, v in d.items():
+        new_key = parent_key + sep + k if parent_key else k
+        if isinstance(v, collections.MutableMapping):
+            items.extend(flatten(v, new_key, sep=sep).items())
+        else:
+            items.append((new_key, v))
+    return dict(items)
