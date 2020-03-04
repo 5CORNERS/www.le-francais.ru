09e6ebd112889259decdfe5400673b2b84b3ed20
Anton Dumov <anton.dumov@gmail.com>
2020-03-04 16:07:47 +0300

    article links final update

--- home/blocks/AlsoReadBlock.py
+++ home/blocks/AlsoReadBlock.py
@@ -1,16 +1,6 @@
 from wagtail.core.blocks import StructBlock, PageChooserBlock, \
-	ListBlock, URLBlock, CharBlock, TextBlock
+	ListBlock, URLBlock, CharBlock, TextBlock, IntegerBlock
 from wagtail.images.blocks import ImageChooserBlock
-
-
-class AlsoReadBlock(ListBlock):
-	def __init__(self):
-		super(AlsoReadBlock, self).__init__(ArticleLink)
-
-	class Meta:
-		template = 'blocks/article_also_read.html'
-
-
 
 class ArticleLink(StructBlock):
 	page = PageChooserBlock(required=False)
@@ -21,13 +11,15 @@ class ArticleLink(StructBlock):
 	image_src = URLBlock(required=False)
 
 	def get_context(self, value, parent_context=None):
-		context = super(ArticleLink, self).get_context(value, parent_context=parent_context)
+		context = super(ArticleLink, self).get_context(value,
+		                                               parent_context=parent_context)
 		if value['page']:
 			context['title'] = value['page'].title
 			if value['page'].specific.reference_title:
 				context['title'] = value['page'].specific.reference_title
 			if value['page'].specific.reference_image:
-				context['image_src'] = value['page'].specific.reference_image.file.url
+				context['image_src'] = value[
+					'page'].specific.reference_image.file.url
 			context['description'] = value['page'].specific.subtitle
 			context['url'] = value['page'].url_path
 		if value['image']:
@@ -43,3 +35,18 @@ class ArticleLink(StructBlock):
 	class Meta:
 		template = 'blocks/article_media_li.html'
 
+
+class ArticleLinks(ListBlock):
+	def __init__(self):
+		super(ArticleLinks, self).__init__(ArticleLink)
+
+	class Meta:
+		template = 'blocks/article_media_ul.html'
+
+class AlsoReadBlock(StructBlock):
+	heading = CharBlock(required=True, default='Читайте также')
+	images_size = IntegerBlock(required=True, default=64)
+	links = ArticleLinks()
+
+	class Meta:
+		template = 'blocks/article_also_read.html'
