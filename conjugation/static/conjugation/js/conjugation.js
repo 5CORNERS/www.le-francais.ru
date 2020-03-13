bff982e2af917fd4cda3520b0fc7f5ac2446bbb2
Anton Dumov <anton.dumov@gmail.com>
2020-03-13 16:41:53 +0300

    conjugation moved css and js into _base.html

--- conjugation/static/conjugation/js/conjugation.js
+++ conjugation/static/conjugation/js/conjugation.js
@@ -34,3 +34,26 @@ window.addEventListener('appinstalled', (evt) => {
 if (window.matchMedia('(display-mode: standalone)').matches || window.navigator.standalone === true) {
   console.log('display-mode is standalone');
 }
+
+$(window).ready(function () {
+  $("#search-verb")["autocomplete"]({
+    source: Urls['conjugation:autocomplete'](),
+    minLength: 1,
+    focus: function (event, ui) {
+      $("#search-verb").val(ui.item["verb"]);
+      return false;
+    },
+    select: function (event, ui) {
+      event.preventDefault();
+      window.location.href = ui.item['url'];
+    }
+  })["data"]("ui-autocomplete")["_renderItem"] = function (a, b) {
+    let liClass = '';
+    if (b['isInfinitive']){
+      liClass = 'is-infinitive'
+    }else if(!b['isInfinitive']){
+      liClass = 'is-not-infinitive'
+    }
+    return $(`<li class='${liClass}'>`)["data"]("ui-menu-item", b)["append"]("<a href='" + b["url"] + "'>" + b["html"] + "</a>")["appendTo"](a);
+  };
+});
