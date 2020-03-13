5944bc2c58e1c9df9c3417482339577cfce9318a
Anton Dumov <anton.dumov@gmail.com>
2020-03-13 19:04:07 +0300

    moved service worker to head, sevice worker clear cache on activate

--- conjugation/static/conjugation/js/conjugation-sw.js
+++ conjugation/static/conjugation/js/conjugation-sw.js
@@ -28,6 +28,19 @@ self.addEventListener('install', function (event) {
     );
 });
 
+self.addEventListener('activate', function(event) {
+  event.waitUntil(
+    caches.keys().then(function(cacheNames) {
+      return Promise.all(
+        cacheNames.filter(function(cacheName) {
+        }).map(function(cacheName) {
+          return caches.delete(cacheName);
+        })
+      );
+    })
+  );
+});
+
 self.addEventListener('fetch', function (event) {
     event.respondWith(
         caches.match(event.request)
--- conjugation/static/conjugation/js/conjugation.js
+++ conjugation/static/conjugation/js/conjugation.js
@@ -1,17 +1,3 @@
-// if ('serviceWorker' in navigator) {
-//   window.addEventListener('load', function() {
-//     navigator.serviceWorker.register('/static/conjugation/js/conjugation-sw.js', {
-//       'scope': '/conjugaison/'
-//     }).then(function(registration) {
-//       // Registration was successful
-//       console.log('ServiceWorker registration successful with scope: ', registration.scope);
-//     }, function(err) {
-//       // registration failed :(
-//       console.log('ServiceWorker registration failed: ', err);
-//     });
-//   });
-// }
-
 let deferredPrompt;
 
 var isTooSoon = true;
