5ff69ca479a2dba5f07c66d8b86d091550dc43ff
Anton Dumov <anton.dumov@gmail.com>
2020-03-17 15:09:34 +0300

    beta21 update

--- le_francais_dictionary/static/dictionary/js/init.js
+++ le_francais_dictionary/static/dictionary/js/init.js
@@ -1,5 +1,5 @@
 (function() {
-	['app.053c4e90.js', 'chunk-vendors.61a3befe.js'].forEach(function (src) {
+	['app.b6f37687.js', 'chunk-vendors.e3473f2e.js'].forEach(function (src) {
 		let script = document.createElement("script");
 		script.setAttribute("src", `/static/dictionary/js/${src}`);
 		script.setAttribute("type", "text/javascript");
@@ -7,7 +7,7 @@
 		document.getElementsByTagName("head")[0].appendChild(script);
 	});
 
-	['app.a8aca2ee.css', 'chunk-vendors.c7c67c91.css'].forEach(function (href) {
+	['app.a8aca2ee.css', 'chunk-vendors.7bcf2756.css'].forEach(function (href) {
 		let script = document.createElement("link");
 		script.setAttribute("href", `/static/dictionary/css/${href}`);
 		script.setAttribute("rel", "stylesheet");
