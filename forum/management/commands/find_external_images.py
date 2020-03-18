853b5b5491fca7e4c933c8e747f1cda88c077317
Anton Dumov <anton.dumov@gmail.com>
2020-03-18 14:44:11 +0300

    update command

--- forum/management/commands/find_external_images.py
+++ forum/management/commands/find_external_images.py
@@ -6,6 +6,7 @@ from django.core.management import BaseCommand
 from django.db.models.signals import post_save
 
 from pybb.models import Post, Topic
+from notifications.models import create_pybb_post_notification
 
 from pandas import read_csv
 
@@ -20,6 +21,7 @@ class Command(BaseCommand):
 		# FIXME: we should have specific signals to send notifications to topic/forum subscribers
 		# but for now, we must connect / disconnect the callback
 		post_save.disconnect(topic_saved, sender=Topic)
+		post_save.disconnect(create_pybb_post_notification, sender=Post)
 		for i in range(len(table)):
 			post_id = int(table['Post ID'][i])
 			old_link = table['Image Link'][i]
@@ -29,6 +31,7 @@ class Command(BaseCommand):
 			post.body = post.body.replace(old_link, new_link)
 			post.save()
 		post_save.connect(topic_saved, sender=Topic)
+		post_save.connect(create_pybb_post_notification, sender=Post)
 
 		for post in Post.objects.all():
 			for link in re.findall('\((((http://)|(https://))(.+?\.(png|jpeg|gif)))', post.body):
@@ -36,7 +39,6 @@ class Command(BaseCommand):
 				file_name = re.sub('/', '+', file_name)
 				if link[0].startswith('https://files.le-francais.ru'):
 					continue
-				s += str(post.id) + ',' + link[0] + ',' + file_name[:] + '\n'
 				image_string = 'https://www.le-francais.ru' + str(post.get_absolute_url()) + '\t' + link[0]
 				file = Path('forum/dat/images/' + file_name[:])
 				if not file.is_file():
@@ -45,7 +47,8 @@ class Command(BaseCommand):
 						opener.addheaders = [('User-Agent', 'Mozilla/5.0')]
 						urllib.request.install_opener(opener)
 						urllib.request.urlretrieve(link[0], 'forum/dat/images/' + file_name[:])
-						print(image_string)
+						print(image_string + ' Success!')
+						s += str(post.id) + ',' + link[0] + ',' + file_name[:] + '\n'
 					except:
 						print(image_string + ' UrlRetrieve Error')
 				else:
