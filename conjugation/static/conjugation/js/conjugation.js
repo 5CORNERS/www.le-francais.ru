783f1258140ea4402bc5f19b9078e02b5c6fb2e7
Anton Dumov <anton.dumov@gmail.com>
2020-03-19 21:40:38 +0300

    autocomplete list hide

--- conjugation/static/conjugation/js/conjugation.js
+++ conjugation/static/conjugation/js/conjugation.js
@@ -36,19 +36,36 @@ if (window.matchMedia('(display-mode: standalone)').matches || window.navigator.
 }
 
 $(window).ready(function () {
+  function showHiddenAutocomplete() {
+    $('.load-more.hide').slideDown(500).removeClass('hide').addClass('show');
+    $('.load-more.action').remove()
+  }
+
   $("#search-verb")["autocomplete"]({
     source: Urls['conjugation:autocomplete'](),
     minLength: 1,
     focus: function (event, ui) {
       $("#search-verb").val(ui.item["verb"]);
+      if(ui.item['cls'].includes('hide')){
+        showHiddenAutocomplete()
+      }
       return false;
     },
     select: function (event, ui) {
       event.preventDefault();
       window.location.href = ui.item['url'];
     },
-    open: function () {
-      $('.ui-menu').width($('#search-verb').innerWidth())
+    open: function (event, ui) {
+      $('.ui-menu').width($('#search-verb').innerWidth()).
+        css('max-height', $(window).height() - $('#search-verb').offset().top - $('#search-verb').height() - 20);
+      if ($('.load-more.hide').length){
+        $('.ui-autocomplete').append($("<li class='verb-autocomplete-item ui-menu-item load-more action'><a class='ui-menu-item-wrapper'>Показать всё</a></li>").on('click', event => {
+          showHiddenAutocomplete()
+        }));
+        $('.ui-autocomplete.hide').on('focus', event => {
+          showHiddenAutocomplete();
+        })
+      }
     }
   })["data"]("ui-autocomplete")["_renderItem"] = function (a, b) {
     let liClass = '';
