be304d2a72f90d22adfae1dc26be7f3864fd7198
Anton Dumov <anton.dumov@gmail.com>
2020-03-26 19:32:01 +0300

    autocomplete styles update

--- conjugation/static/conjugation/js/conjugation-sw.js
+++ conjugation/static/conjugation/js/conjugation-sw.js
@@ -1,4 +1,4 @@
-var CACHE_NAME = 'le_francais-conjugation-cache-v8';
+var CACHE_NAME = 'le_francais-conjugation-cache-v9';
 var urlsToCache = [
     '/static/conjugation/css/conjugation.min.css',
     '/static/conjugation/js/conjugation.min.js',
