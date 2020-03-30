e588a021be6c56fd58ac0479adc61d15cee66a66
Anton Dumov <anton.dumov@gmail.com>
2020-03-30 18:24:38 +0300

    mass_mailer update

--- mass_mailer/admin.py
+++ mass_mailer/admin.py
@@ -1,18 +1,21 @@
 from django.contrib import admin
 
 # Register your models here.
-from mass_mailer.models import Message, EmailSettings, UsersFilter, Profile
+from mass_mailer.models import Message, EmailSettings, UsersFilter, Profile, MessageLog
 
 
 def send(modeladmin, request, qs):
 	for q in qs:
 		q.send()
 
+def clear_sent(modeladmin, request, qs):
+	for q in qs:
+		q.sent.clear()
 
 @admin.register(Message)
 class MessageAdmin(admin.ModelAdmin):
 	readonly_fields = ['sent']
-	actions = [send]
+	actions = [send, clear_sent]
 
 @admin.register(EmailSettings)
 class EmailSettings(admin.ModelAdmin):
@@ -25,4 +28,8 @@ class UsersFilterAdmin(admin.ModelAdmin):
 @admin.register(Profile)
 class MailerProfileAdmin(admin.ModelAdmin):
 	raw_id_fields = ['user']
+	pass
+
+@admin.register(MessageLog)
+class MessageLogAdmin(admin.ModelAdmin):
 	pass
