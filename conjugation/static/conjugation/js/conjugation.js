921b79ef5b0bce2f9375845944d72bc7066abbd5
Anton Dumov <anton.dumov@gmail.com>
2020-04-10 16:02:24 +0300

    restructure autocomplete function; change minlength to 0; change source to predefined verbs if length of input val is none.

--- conjugation/static/conjugation/js/conjugation.js
+++ conjugation/static/conjugation/js/conjugation.js
@@ -1,4 +1,5 @@
 let deferredPrompt;
+let $searchVerb;
 
 if ('serviceWorker' in navigator) {
   window.addEventListener('load', function () {
@@ -41,39 +42,150 @@ $(window).ready(function () {
     $('.load-more.action').remove()
   }
 
-  $("#search-verb")["autocomplete"]({
-    source: Urls['conjugation:autocomplete'](),
-    minLength: 1,
-    focus: function (event, ui) {
-      $("#search-verb").val(ui.item["verb"]);
-      if(ui.item['cls'].includes('hide')){
-        showHiddenAutocomplete()
-      }
-      return false;
-    },
-    select: function (event, ui) {
-      event.preventDefault();
-      window.location.href = ui.item['url'];
-    },
-    open: function (event, ui) {
-      $('.ui-menu').width($('#search-verb').innerWidth()).
-        css('max-height', $(window).height() - $('#search-verb').offset().top - $('#search-verb').height() - 20);
-      if ($('.load-more.hide').length){
-        $('.ui-autocomplete').append($("<li class='verb-autocomplete-item ui-menu-item load-more action'><a class='ui-menu-item-wrapper'>Показать ещё</a></li>").on('click', event => {
+
+    $searchVerb = $("#search-verb").autocomplete({
+      source: Urls['conjugation:autocomplete'](),
+      minLength: 0,
+      focus: function (event, ui) {
+        $("#search-verb").val(ui.item["verb"]);
+        if (ui.item['cls'].includes('hide')) {
           showHiddenAutocomplete()
-        }));
-        $('.ui-autocomplete.hide').on('focus', event => {
-          showHiddenAutocomplete();
-        })
+        }
+        return false;
+      },
+      select: function (event, ui) {
+        event.preventDefault();
+        window.location.href = ui.item['url'];
+      },
+      open: function (event, ui) {
+        $('.ui-menu').width($('#search-verb').innerWidth()).css('max-height', $(window).height() - $('#search-verb').offset().top - $('#search-verb').height() - 20);
+        if ($('.load-more.hide').length) {
+          $('.ui-autocomplete').append($("<li class='verb-autocomplete-item ui-menu-item load-more action'><a class='ui-menu-item-wrapper'>Показать ещё</a></li>").on('click', event => {
+            showHiddenAutocomplete()
+          }));
+          $('.ui-autocomplete.hide').on('focus', event => {
+            showHiddenAutocomplete();
+          })
+        }
+      },
+      search: function (event, ui) {
+        if (event.target.value === ""){
+          $searchVerb.autocomplete('option', 'source', [
+            {
+              "url": "/conjugaison/etre/",
+              "verb": "être",
+              "html": "<b></b>être",
+              "isInfinitive": true,
+              "cls": "starts-with-infinitive"
+            },
+            {
+              "url": "/conjugaison/avoir/",
+              "verb": "avoir",
+              "html": "<b></b>avoir",
+              "isInfinitive": true,
+              "cls": "starts-with-infinitive"
+            },
+            {
+              "url": "/conjugaison/aller/",
+              "verb": "aller",
+              "html": "<b></b>aller",
+              "isInfinitive": true,
+              "cls": "starts-with-infinitive"
+            },
+            {
+              "url": "/conjugaison/faire/",
+              "verb": "faire",
+              "html": "<b></b>faire",
+              "isInfinitive": true,
+              "cls": "starts-with-infinitive"
+            },
+            {
+              "url": "/conjugaison/venir/",
+              "verb": "venir",
+              "html": "<b></b>venir",
+              "isInfinitive": true,
+              "cls": "starts-with-infinitive"
+            },
+            {
+              "url": "/conjugaison/prendre/",
+              "verb": "prendre",
+              "html": "<b></b>prendre",
+              "isInfinitive": true,
+              "cls": "starts-with-infinitive"
+            },
+            {
+              "url": "/conjugaison/appeler/",
+              "verb": "appeler",
+              "html": "<b></b>appeler",
+              "isInfinitive": true,
+              "cls": "starts-with-infinitive"
+            },
+            {
+              "url": "/conjugaison/pouvoir/",
+              "verb": "pouvoir",
+              "html": "<b></b>pouvoir",
+              "isInfinitive": true,
+              "cls": "starts-with-infinitive"
+            },
+            {
+              "url": "/conjugaison/vouloir/",
+              "verb": "vouloir",
+              "html": "<b></b>vouloir",
+              "isInfinitive": true,
+              "cls": "starts-with-infinitive"
+            },
+            {
+              "url": "/conjugaison/voir/",
+              "verb": "voir",
+              "html": "<b></b>voir",
+              "isInfinitive": true,
+              "cls": "starts-with-infinitive"
+            },
+            {
+              "url": "/conjugaison/partir/",
+              "verb": "partir",
+              "html": "<b></b>partir",
+              "isInfinitive": true,
+              "cls": "starts-with-infinitive"
+            },
+            {
+              "url": "/conjugaison/mettre/",
+              "verb": "mettre",
+              "html": "<b></b>mettre",
+              "isInfinitive": true,
+              "cls": "starts-with-infinitive"
+            },
+            {
+              "url": "/conjugaison/dire/",
+              "verb": "dire",
+              "html": "<b></b>dire",
+              "isInfinitive": true,
+              "cls": "starts-with-infinitive"
+            },
+            {
+              "url": "/conjugaison/parler/",
+              "verb": "parler",
+              "html": "<b></b>parler",
+              "isInfinitive": true,
+              "cls": "starts-with-infinitive"
+            },
+          ]);
+        }else{
+          $searchVerb.autocomplete('option', 'source', Urls['conjugation:autocomplete']());
+        }
       }
-    }
-  })["data"]("ui-autocomplete")["_renderItem"] = function (a, b) {
-    let liClass = '';
-    if (b['isInfinitive']){
-      liClass = 'is-infinitive'
-    }else if(!b['isInfinitive']){
-      liClass = 'is-not-infinitive'
-    }
-    return $(`<li class='verb-autocomplete-item ${liClass} ${b["cls"]}'>`)["data"]("ui-menu-item", b)["append"]("<a href='" + b["url"] + "'>" + b["html"] + "</a>")["appendTo"](a);
-  };
+    });
+    $searchVerb.data('ui-autocomplete')._renderItem = function (a, b) {
+      let liClass = '';
+      if (b['isInfinitive']) {
+        liClass = 'is-infinitive'
+      } else if (!b['isInfinitive']) {
+        liClass = 'is-not-infinitive'
+      }
+      return $(`<li class='verb-autocomplete-item ${liClass} ${b["cls"]}'>`)["data"]("ui-menu-item", b)["append"]("<a href='" + b["url"] + "'>" + b["html"] + "</a>")["appendTo"](a);
+    };
+    $searchVerb.on('focus', e=>{
+      console.log('Focused on input');
+      $searchVerb.autocomplete('search', '');
+    })
 });
