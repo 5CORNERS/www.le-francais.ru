01201a1d259afa96861bc03a22d22b75344e706b
Anton Dumov <anton.dumov@gmail.com>
2020-05-02 17:38:01 +0300

    confirm post if user has at least one confirmed post

--- profiles/utils.py
+++ profiles/utils.py
@@ -1,6 +1,8 @@
 def check_user(user, post):
-    if user.is_superuser:
-        return True
     if user.days_since_joined():
 	    return True
+    if user.posts.filter(on_moderation=False).count() > 1:
+	    return True
+    if user.is_superuser:
+        return True
     return False
