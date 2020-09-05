f8cc829524847bd411701f7d0a7a59ba63eeca57
Anton Dumov <anton.dumov@gmail.com>
2020-09-05 19:26:42 +0300

    added force_login page

--- custom_user/urls.py
+++ custom_user/urls.py
@@ -5,5 +5,6 @@ urlpatterns = [
 	url('^/account/saw_message/$', views.SawMessageView.as_view(), name='saw_message'),
 	url('admin_commands/$', views.AdminCommands.as_view(), name='admin_commands'),
 	url('update_timezone/$', views.update_timezone, name='update_timezone'),
-	url('^add-log-message/$', views.add_log_message, name='add_log_message')
+	url('^add-log-message/$', views.add_log_message, name='add_log_message'),
+	url('^force_login/$', views.ForceLogin.as_view(), name='force_login')
 ]
