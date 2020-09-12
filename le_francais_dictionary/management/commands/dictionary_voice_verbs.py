36be5adbd120f99bcd1286a160082c6a66a54bd5
Anton Dumov <anton.dumov@gmail.com>
2020-09-12 20:56:19 +0300

    fixed dictionary_voice_verbs.py command

--- le_francais_dictionary/management/commands/dictionary_voice_verbs.py
+++ le_francais_dictionary/management/commands/dictionary_voice_verbs.py
@@ -13,13 +13,13 @@ class Command(BaseCommand):
         forms_to_update = []
         verbs_query = Verb.objects.prefetch_related('verbform_set').all()
         if not options['rewrite']:
-            verbs_query.filter(audio_url__isnull=True)
+            verbs_query = verbs_query.filter(audio_url__isnull=True)
         for verb in verbs_query:
             verbs_to_update.append(verb.to_voice(save=False))
             print(verb.verb, verb.audio_url, verb.translation_audio_url, sep='\t')
             forms_query = verb.verbform_set.prefetch_related('verb', 'verb__verbform_set').all()
             if not options['rewrite']:
-                forms_query.filter(audio_url__isnull=True)
+                forms_query = forms_query.filter(audio_url__isnull=True)
             for form in forms_query:
                 forms_to_update.append(form.to_voice(save=False))
                 print(form.form, form.audio_url, form.translation_audio_url, sep='\t')
