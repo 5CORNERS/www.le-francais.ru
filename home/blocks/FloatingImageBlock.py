1dbf765cd151881b6159fb7c14897fbcffeb410c
Anton Dumov <anton.dumov@gmail.com>
2020-09-14 22:34:04 +0300

    update

--- home/blocks/FloatingImageBlock.py
+++ home/blocks/FloatingImageBlock.py
@@ -1,6 +1,7 @@
 from wagtail.core.blocks import StreamBlock, StructBlock, RichTextBlock, \
 	RawHTMLBlock, TextBlock, ChoiceBlock, IntegerBlock, CharBlock
 from wagtail.images.blocks import ImageChooserBlock
+from wagtail.images.templatetags.wagtailimages_tags import ImageNode
 
 
 class FloatingImageBlock(StructBlock):
@@ -23,6 +24,18 @@ class FloatingImageBlock(StructBlock):
 		context = super(FloatingImageBlock, self).get_context(value, parent_context=None)
 		context['value']['lg_image_attr'] = 'width-' + str(value['width_large'])
 		context['value']['sm_image_attr'] = 'width-' + str(value['width_small'])
+		context['value']['lg_image'] = ImageNode(
+			context['value']['image'],
+			context['value']['lg_image_attr'],
+			attrs={},
+			output_var_name='lg_image'
+		)
+		context['value']['sm_image'] = ImageNode(
+			context['value']['image'],
+			context['value']['sm_image_attr'],
+			attrs={},
+			output_var_name='sm_image'
+		)
 		return context
 
 	class Meta:
