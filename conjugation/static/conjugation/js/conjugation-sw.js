dc2e058464d5d3df688c64f9c7c24806b7f018c9
Anton Dumov <anton.dumov@gmail.com>
2020-09-22 21:21:57 +0300

    conjugation styles

--- conjugation/static/conjugation/js/conjugation-sw.js
+++ conjugation/static/conjugation/js/conjugation-sw.js
@@ -1,4 +1,4 @@
-var CACHE_NAME = 'le_francais-conjugation-cache-v15';
+var CACHE_NAME = 'le_francais-conjugation-cache-v16';
 var urlsToCache = [
     '/static/conjugation/css/conjugation.min.css',
     '/static/conjugation/js/conjugation.min.js',
