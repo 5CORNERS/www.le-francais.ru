3c0c3852b214df072f1b20f686154a1322ecc658
Anton Dumov <anton.dumov@gmail.com>
2021-03-02 19:31:19 +0300

    fixing button

--- le_francais_dictionary/static/dictionary/js/init.js
+++ le_francais_dictionary/static/dictionary/js/init.js
@@ -1,5 +1,5 @@
 (function() {
-	['%APP_JS%', '%APP_BUNDLE_JS%'].forEach(function (src) {
+	['app.js', 'chunk-vendors.js'].forEach(function (src) {
 		let script = document.createElement("script");
 		script.setAttribute("src", `/static/dictionary/js/${src}`);
 		script.setAttribute("type", "text/javascript");
@@ -7,7 +7,7 @@
 		document.getElementsByTagName("head")[0].appendChild(script);
 	});
 
-	['%APP_CSS%', '%APP_BUNDLE_CSS%'].forEach(function (href) {
+	['app.css', 'chunk-vendors.css'].forEach(function (href) {
 		let script = document.createElement("link");
 		script.setAttribute("href", `/static/dictionary/css/${href}`);
 		script.setAttribute("rel", "stylesheet");
