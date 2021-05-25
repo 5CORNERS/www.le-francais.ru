9e189d98dba683317de0d99bae04bb0bed79ca24
Anton Dumov <anton.dumov@gmail.com>
2021-05-25 22:45:49 +0300

    init

---
+++ donations/static/donations/script.js
@@ -0,0 +1,31 @@
+$(document).ready(() => {
+    let $typePills = $('#donationFormTypePills .nav-link')
+    let $typeInput = $('#donationFormType')
+    $typePills.click(e => {
+        e.preventDefault()
+        $typePills.toggleClass('active')
+        $typeInput.val($(e.target).data('value'))
+        console.log($(e.target).data('value'))
+    })
+
+    let $amountPills = $('#donationsFormAmountPills .nav-link')
+    let $amountDummyInput = $('#donationsFormAmountPillsCustom')
+    let $amountInput = $('#donationFormAmount')
+    $amountPills.click(e => {
+        e.preventDefault()
+        let $this = $(e.currentTarget)
+        $amountPills.removeClass('active')
+        $this.addClass('active')
+        if ($this.data('value')){
+            $amountInput.val($this.data('value'))
+            console.log($this.data('value'))
+        }else{
+            $amountInput.val($(e.target).val())
+            console.log($(e.target).val())
+        }
+    })
+    $amountDummyInput.change(e => {
+        $amountInput.val($(e.target).val())
+        console.log($(e.target).val())
+    })
+})
