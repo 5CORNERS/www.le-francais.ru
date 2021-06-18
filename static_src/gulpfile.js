9eb4ef5a9468abe4ca824f5e0b6d338e6b9d0740
Anton Dumov <anton.dumov@gmail.com>
2021-06-18 19:22:30 +0300

    init

--- static_src/gulpfile.js
+++ static_src/gulpfile.js
@@ -1,8 +1,9 @@
 const gulp = require('gulp');
 const webpack = require('webpack-stream');
-const sass = require('gulp-sass');
-const uglify = require('gulp-uglify');
-const minify = require('gulp-minify-css');
+const sass = require('gulp-dart-sass');
+const closureCompiler = require('google-closure-compiler').gulp();
+const sourcemaps = require('gulp-sourcemaps');
+const minify = require('gulp-clean-css');
 const del = require('del');
 const dist = '../static_compiled/components';
 
@@ -15,30 +16,99 @@ gulp.task('sass', () => {
 
 gulp.task('css', () => {
     return gulp.src([
-        'node_modules/vue-multiselect/dist/vue-multiselect.min.css',
         'css/**/*.css',
+        './node_modules/jquery-ui-dist/jquery-ui.min.css'
     ])
         .pipe(minify())
         .pipe(gulp.dest(dist + '/css/'))
 });
 
-gulp.task('js', () => {
+const base_js_modules_src = [
+    './node_modules/jquery/dist/jquery.js',
+    './node_modules/bootstrap/dist/js/bootstrap.js',
+    './node_modules/js-cookie/src/js.cookie.js',
+    './node_modules/popper.js/dist/popper.js',
+    './node_modules/moment/moment.js',
+    './js/base/**/*.js'
+]
+
+gulp.task('js-base', () => {
+    return gulp.src(base_js_modules_src)
+        .pipe(closureCompiler({
+            compilation_level: 'SIMPLE_OPTIMIZATIONS',
+            output_wrapper: '(function(){\n%output%\n}).call(this)',
+            js_output_file: 'base.min.js'
+        }))
+        .pipe(gulp.dest(dist + '/js/base/'))
+})
+
+gulp.task('js-lessons', () => {
+    return gulp.src(base_js_modules_src.concat([
+        './node_modules/bootstrap-treeview/dist/bootstrap-treeview.min.js',
+        './js/lessons/**/*.js'
+    ]))
+        .pipe(closureCompiler({
+            compilation_level: 'SIMPLE_OPTIMIZATIONS',
+            output_wrapper: '(function(){\n%output%\n}).call(this)',
+            js_output_file: 'lessons.min.js'
+        }))
+        .pipe(gulp.dest(dist + '/js/lessons/'))
+})
+
+gulp.task('js-footable', () => {
+    return gulp.src(base_js_modules_src.concat([
+        './node_modules/bootstrap-select/dist/js/bootstrap-select.js',
+        './node_modules/jquery.simple-checkbox-table/dist/jquery.simple-checkbox-table.js',
+        './js/footable/**/*.js'
+    ]))
+        .pipe(closureCompiler({
+            compilation_level: 'SIMPLE_OPTIMIZATIONS',
+            output_wrapper: '(function(){\n%output%\n}).call(this)',
+            js_output_file: 'table.min.js'
+        }))
+        .pipe(gulp.dest(dist + '/js/footable/'))
+})
+
+gulp.task('js-my-words', () => {
+    return gulp.src(base_js_modules_src.concat([
+        './node_modules/bootstrap-select/dist/js/bootstrap-select.js',
+        './node_modules/jquery.simple-checkbox-table/dist/jquery.simple-checkbox-table.js',
+        './js/my_words/**/*.js'
+    ]))
+        .pipe(closureCompiler({
+            compilation_level: 'SIMPLE_OPTIMIZATIONS',
+            output_wrapper: '(function(){\n%output%\n}).call(this)',
+            js_output_file: 'my_words.min.js'
+        }))
+        .pipe(gulp.dest(dist + '/js/my_words/'))
+})
+
+gulp.task('js-conjugations', () => {
+    return gulp.src(base_js_modules_src.concat([
+        './js/conjugations/**/*.js',
+        './node_modules/jquery-ui-dist/jquery-ui.js'
+    ]))
+        .pipe(closureCompiler({
+            compilation_level: 'SIMPLE_OPTIMIZATIONS',
+            output_wrapper: '(function(){\n%output%\n}).call(this)',
+            js_output_file: 'conjugations.min.js'
+        }))
+        .pipe(gulp.dest(dist + '/js/conjugations/'))
+})
+
+gulp.task('js-lesson-verbs', () => {
     return gulp.src([
-        'node_modules/jquery/dist/jquery.js',
-        'node_modules/bootstrap/dist/js/bootstrap.js',
-        'node_modules/bootstrap-treeview/dist/bootstrap-treeview.min.js',
-        'node_modules/bootstrap-select/dist/js/bootstrap-select.js',
-        'node_modules/jquery.simple-checkbox-table/dist/jquery.simple-checkbox-table.js',
-        'node_modules/moment/moment.js',
-        'node_modules/js-cookie/src/js.cookie.js',
-        'node_modules/vue/dist/vue.js',
-        'node_modules/vue-multiselect/dist/vue-multiselect.min.js',
-        'node_modules/howler/dist/howler.js',
-        'src/js/**/*.js'
+        './node_modules/vue/dist/vue.js',
+        './node_modules/howler/dist/howler.js',
+        './js/lessons_verbs/**/*.js'
     ])
-        .pipe(uglify())
-        .pipe(gulp.dest(dist + '/js/'))
-});
+        .pipe(closureCompiler({
+            compilation_level: 'SIMPLE_OPTIMIZATIONS',
+            output_wrapper: '(function(){\n%output%\n}).call(this)',
+            js_output_file: 'lesson_verbs.min.js'
+        }))
+        .pipe(gulp.dest(dist + '/js/lesson_verbs/'))
+})
 
 gulp.task('fa-fonts', function () {
     return gulp.src('node_modules/@fortawesome/fontawesome-free/webfonts/*')
@@ -51,4 +121,14 @@ gulp.task('clean', () => {
     ], {force: true});
 });
 
-gulp.task('default', gulp.series(['clean', 'sass', 'css', 'js', 'fa-fonts']));
+gulp.task('default', gulp.series([
+    'clean',
+    'sass',
+    'css',
+    'js-base',
+    'js-lessons',
+    'js-conjugations',
+    'js-footable',
+    'js-my-words',
+    'js-lesson-verbs',
+    'fa-fonts']));
---
+++ static_src/js/conjugations/conjugation.js
@@ -0,0 +1,187 @@
+let deferredPrompt;
+let $searchVerb;
+
+if ('serviceWorker' in navigator) {
+  window.addEventListener('load', function () {
+    navigator.serviceWorker.register('/static/conjugation/js/conjugation-sw.js', {
+      'scope': '/conjugaison/'
+    }).then(function (registration) {
+      // Registration was successful
+      console.log('ServiceWorker registration successful with scope: ', registration.scope);
+    }, function (err) {
+      // registration failed :(
+      console.log('ServiceWorker registration failed: ', err);
+    });
+  });
+}
+
+var isTooSoon = true;
+window.addEventListener("beforeinstallprompt", function(e) {
+  deferredPrompt = e;
+  if (isTooSoon) {
+    deferredPrompt.preventDefault(); // Prevents prompt display
+    // Prompt later instead:
+    setTimeout(function() {
+      isTooSoon = false;
+      deferredPrompt.prompt(); // Throws if called more than once or default not prevented
+    }, 10000);
+  }
+});
+
+window.addEventListener('appinstalled', (evt) => {
+  console.log('a2hs installed');
+});
+
+if (window.matchMedia('(display-mode: standalone)').matches || window.navigator.standalone === true) {
+  console.log('display-mode is standalone');
+}
+
+$(window).ready(function () {
+  function showHiddenAutocomplete() {
+    $('.load-more.hide').slideDown(500).removeClass('hide').addClass('show');
+    $('.load-more.action').remove()
+  }
+
+
+    $searchVerb = $("#search-verb").autocomplete({
+      source: Urls['conjugation:autocomplete'](),
+      minLength: 0,
+      focus: function (event, ui) {
+        if (event.originalEvent.originalEvent.type === 'keydown') {
+          $("#search-verb").val(ui.item["verb"]);
+        }
+        if (ui.item['cls'].includes('hide')) {
+          showHiddenAutocomplete()
+        }
+        return false;
+      },
+      select: function (event, ui) {
+        event.preventDefault();
+        window.location.href = ui.item['url'];
+      },
+      open: function (event, ui) {
+        $('.ui-menu').width($('#search-verb').innerWidth()).css('max-height', $(window).height() - $('#search-verb').offset().top - $('#search-verb').height() - 20);
+        if ($('.load-more.hide').length) {
+          $('.ui-autocomplete').append($("<li class='verb-autocomplete-item ui-menu-item load-more action'><a class='ui-menu-item-wrapper'>Показать ещё</a></li>").on('click', event => {
+            showHiddenAutocomplete()
+          }));
+          $('.ui-autocomplete.hide').on('focus', event => {
+            showHiddenAutocomplete();
+          })
+        }
+      },
+      search: function (event, ui) {
+        if (event.target.value === ""){
+          $searchVerb.autocomplete('option', 'source', [
+            {
+              "url": "/conjugaison/etre/",
+              "verb": "être",
+              "html": "<b></b>être",
+              "isInfinitive": true,
+              "cls": "starts-with-infinitive"
+            },
+            {
+              "url": "/conjugaison/avoir/",
+              "verb": "avoir",
+              "html": "<b></b>avoir",
+              "isInfinitive": true,
+              "cls": "starts-with-infinitive"
+            },
+            {
+              "url": "/conjugaison/aller/",
+              "verb": "aller",
+              "html": "<b></b>aller",
+              "isInfinitive": true,
+              "cls": "starts-with-infinitive"
+            },
+            {
+              "url": "/conjugaison/faire/",
+              "verb": "faire",
+              "html": "<b></b>faire",
+              "isInfinitive": true,
+              "cls": "starts-with-infinitive"
+            },
+            {
+              "url": "/conjugaison/venir/",
+              "verb": "venir",
+              "html": "<b></b>venir",
+              "isInfinitive": true,
+              "cls": "starts-with-infinitive"
+            },
+            {
+              "url": "/conjugaison/prendre/",
+              "verb": "prendre",
+              "html": "<b></b>prendre",
+              "isInfinitive": true,
+              "cls": "starts-with-infinitive"
+            },
+            {
+              "url": "/conjugaison/appeler/",
+              "verb": "appeler",
+              "html": "<b></b>appeler",
+              "isInfinitive": true,
+              "cls": "starts-with-infinitive"
+            },
+            {
+              "url": "/conjugaison/pouvoir/",
+              "verb": "pouvoir",
+              "html": "<b></b>pouvoir",
+              "isInfinitive": true,
+              "cls": "starts-with-infinitive"
+            },
+            {
+              "url": "/conjugaison/vouloir/",
+              "verb": "vouloir",
+              "html": "<b></b>vouloir",
+              "isInfinitive": true,
+              "cls": "starts-with-infinitive"
+            },
+            {
+              "url": "/conjugaison/voir/",
+              "verb": "voir",
+              "html": "<b></b>voir",
+              "isInfinitive": true,
+              "cls": "starts-with-infinitive"
+            },
+            {
+              "url": "/conjugaison/partir/",
+              "verb": "partir",
+              "html": "<b></b>partir",
+              "isInfinitive": true,
+              "cls": "starts-with-infinitive"
+            },
+            {
+              "url": "/conjugaison/mettre/",
+              "verb": "mettre",
+              "html": "<b></b>mettre",
+              "isInfinitive": true,
+              "cls": "starts-with-infinitive"
+            },
+            {
+              "url": "/conjugaison/dire/",
+              "verb": "dire",
+              "html": "<b></b>dire",
+              "isInfinitive": true,
+              "cls": "starts-with-infinitive"
+            },
+          ]);
+        }else{
+          $searchVerb.autocomplete('option', 'source', Urls['conjugation:autocomplete']());
+        }
+      }
+    });
+    $searchVerb.data('ui-autocomplete')._renderItem = function (a, b) {
+      let liClass = '';
+      if (b['isInfinitive']) {
+        liClass = 'is-infinitive'
+      } else if (!b['isInfinitive']) {
+        liClass = 'is-not-infinitive'
+      }
+      return $(`<li class='verb-autocomplete-item ${liClass} ${b["cls"]}'>`
+      )["data"]("ui-menu-item", b)["append"]("<a href='" + b["url"] + "'>" + b["html"] + "</a>")["appendTo"](a);
+    };
+    $searchVerb.on('focus', e=>{
+      console.log('Focused on input');
+      $searchVerb.autocomplete('search', '');
+    })
+});
