eed1e5bc8abcd01073772e1d54406a6a39392693
Anton Dumov <anton.dumov@gmail.com>
2021-11-09 20:09:37 +0300

    adding utm fields

--- ads/urls.py
+++ ads/urls.py
@@ -4,7 +4,7 @@ from django.views.generic import TemplateView
 from . import views
 
 urlpatterns = [
-    url(r'^(?P<line_item>\d+)/(?P<creative>\d+)/$',
+    url(r'^(?P<line_item>\d+)/(?P<creative>\d+)/(?P<source>.+)/$',
         views.AdCounterRedirectView.as_view(),
         name='ad-counter-redirect'),
     url(r'^test/$', views.TestView.as_view())
