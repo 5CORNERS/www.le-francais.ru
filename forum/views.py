5ca8c299350d3986b25d9ab596b528feaf6d73d7
Anton Dumov <anton.dumov@gmail.com>
2021-11-23 13:34:33 +0300

    added median to forum statistics page

--- forum/views.py
+++ forum/views.py
@@ -1,4 +1,5 @@
 import datetime
+import statistics
 
 from dateutil import relativedelta
 from django.shortcuts import render
@@ -39,8 +40,12 @@ def statistics_page(request):
     while date_range[1] > first_post_date:
         posts = list(filter(lambda x: date_range[0] <= x.created <= date_range[1], all_posts))
         users = list(set([post.user_id for post in posts]))
+        users_post = {user_id: [post for post in posts if
+                                post.user_id == user_id] for user_id
+                      in users}
         result_by_weeks.append(
-            {'range': date_range, 'users_count': len(users), 'posts_count': len(posts)}
+            {'range': date_range, 'users_count': len(users), 'posts_count': len(posts),
+             'users_post_median': statistics.median([len(user_posts) for user_posts in users_post.values()]) if users_post.values() else 0}
         )
         date_range = (date_range[0]-datetime.timedelta(days=7), date_range[1]-datetime.timedelta(days=7))
     context['posts_statistics_by_weeks'] = result_by_weeks
@@ -48,13 +53,17 @@ def statistics_page(request):
     result_by_months = []
     current_year, current_month = today.year, today.month
     while current_year >= first_post_date.year or current_month >= first_post_date.month:
-        posts = [post for post in all_posts if post.created.month == current_month and post.created.year == current_year]
+        posts = [post for post in all_posts if
+                 post.created.month == current_month and post.created.year == current_year]
         users = list(set([post.user_id for post in posts]))
+        users_post = {user_id:[post for post in posts if post.user_id == user_id] for user_id in users}
         result_by_months.append(
             {'year': current_year, 'month': current_month,
-             'users_count': len(users), 'posts_count': len(posts)}
+             'users_count': len(users), 'posts_count': len(posts),
+             'users_post_median': statistics.median([len(user_posts) for user_posts in users_post.values()]) if users_post.values() else 0}
         )
-        next_date = (datetime.date(year=current_year, month=current_month, day=1) - datetime.timedelta(days=1)).replace(day=1)
+        next_date = (datetime.date(year=current_year, month=current_month, day=1)
+                     - datetime.timedelta(days=1)).replace(day=1)
         current_year = next_date.year
         current_month = next_date.month
     context['posts_statistics_by_months'] = result_by_months
