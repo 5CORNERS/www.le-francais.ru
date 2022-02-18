f26c3f9f4d91ed516902001b77ae293461776f58
Anton Dumov <anton.dumov@gmail.com>
2022-02-18 21:34:52 +0300

    fixed checking showdeleted

--- le_francais_dictionary/static/dictionary/js/user-table-standalone.js
+++ le_francais_dictionary/static/dictionary/js/user-table-standalone.js
@@ -1,5 +1,7 @@
 $(document).ready(function () {
     $(document).on('tableComplete', function () {
+        $('#showDeleted').prop('checked', true);
+        // $('#showDeleted')[0].checked = true;
         showDeleted(true);
         visibleCheckboxes(false);
         if (USER_IS_AUTHENTICATED === false){
