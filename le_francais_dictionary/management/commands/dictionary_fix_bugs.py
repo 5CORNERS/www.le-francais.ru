266ac813fb754ee21e1698c31a1a4af5f5664f40
Anton Dumov <anton.dumov@gmail.com>
2022-02-22 15:36:06 +0300

    fixed filename error

--- le_francais_dictionary/management/commands/dictionary_fix_bugs.py
+++ le_francais_dictionary/management/commands/dictionary_fix_bugs.py
@@ -79,7 +79,7 @@ class Command(BaseCommand):
                 word.genre = GENRE_FEMININE
             elif details == DETAILS_FEMININE_TO_MASCULINE and word.genre == GENRE_FEMININE:
                 word.genre = GENRE_MASCULINE
-            if row[COL_FR_MP3] or row[COL_FR_REWRITE] or row[COL_FR_SYNTH_STRING] or place in [PLACE_FR, PLACE_RU_FR]:
+            if row[COL_FR_MP3] or row[COL_FR_REWRITE] or row[COL_FR_SYNTH_STRING] or place in [PLACE_FR, PLACE_RU_FR] or row[COL_GENRE]:
                 if row[COL_FR_REWRITE]:
                     word.word = row[COL_FR_REWRITE]
                 if row[COL_FR_SYNTH_STRING]:
@@ -98,6 +98,8 @@ class Command(BaseCommand):
                         new_file.close()
                     url = FR_WORDS_URL + row[COL_FR_MP3]
                     word._polly_url = url
+                if row[COL_GENRE]:
+                    word.genre = row[COL_GENRE]
                 word.save()
 
             if row[COL_RU_REWRITE] or row[COL_RU_SYNTH_STRING] or row[COL_RU_MP3] or place in [PLACE_RU, PLACE_RU_FR]:
@@ -121,7 +123,3 @@ class Command(BaseCommand):
                     url = RU_WORDS_URL + row[COL_RU_MP3]
                     translation._polly_url = url
                 translation.save()
-
-            if row[COL_GENRE]:
-                word.genre = row[COL_GENRE]
-                word.save()
