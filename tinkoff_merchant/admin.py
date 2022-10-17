f25c55a8b5608656cb248a4b4e9ced338216b124
Anton Dumov <anton.dumov@gmail.com>
2022-10-17 18:40:17 +0300

    added get_status function to tinkoff admin

--- tinkoff_merchant/admin.py
+++ tinkoff_merchant/admin.py
@@ -1,8 +1,28 @@
+import bulk_update.helper
 from django.contrib import admin
 
 from .models import Payment, Receipt, ReceiptItem
 from .services import MerchantAPI
+from .consts import PAYMENT_PAYED_STATUSES, PAYMENT_REFUNDED_STATUSES
+from .signals import payment_confirm, payment_refund
 
+
+def make_get_status(modeladmin: admin.ModelAdmin, request, qs):
+    to_update = []
+    for p in qs:
+        p:Payment
+        api = MerchantAPI()
+        old_status = p.status
+        p = api.status(p)
+
+        if old_status not in PAYMENT_PAYED_STATUSES and p.status in PAYMENT_PAYED_STATUSES:
+            payment_confirm.send(modeladmin, payment=p)
+        if old_status not in PAYMENT_REFUNDED_STATUSES and p.status in PAYMENT_REFUNDED_STATUSES:
+            payment_refund.send(modeladmin, payment=p)
+
+        to_update.append(p)
+
+    bulk_update.helper.bulk_update(to_update)
 
 def make_cancel(modeladmin: admin.ModelAdmin, request, qs):
     for p in qs:
@@ -17,7 +37,7 @@ class PaymentAdmin(admin.ModelAdmin):
     list_display = ['id', 'order_id', 'get_amount_rub', 'success', 'status', 'payment_id', 'creation_date', 'update_date', 'email', 'recurrent', 'rebill_id', 'username']
     list_filter = ['status', 'success', 'recurrent']
     search_fields = ['order_id', 'payment_id', 'rebill_id']
-    actions = [make_cancel]
+    actions = [make_cancel, make_get_status]
 
     def get_amount_rub(self, obj):
         return obj.amount / 100
