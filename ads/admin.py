2640ecbf775ccc2ff7d92e683391919789ee5362
Anton Dumov <anton.dumov@gmail.com>
2022-10-27 19:58:39 +0300

    updating ads models

--- ads/admin.py
+++ ads/admin.py
@@ -1,22 +1,27 @@
 from django.contrib import admin
 
 # Register your models here.
+from ads.forms import GeoAdder
 from ads.models import LineItem, Placement, Creative
 
 
 class CreativeInline(admin.TabularInline):
     model = Creative
     fk_name = 'line_item'
-    extra = 5
-    readonly_fields = ['views', 'clicks']
+    extra = 0
+    readonly_fields = ['views', 'clicks', 'width', 'height']
 
 
 @admin.register(LineItem)
 class LineItemAdmin(admin.ModelAdmin):
     readonly_fields = ['views', 'clicks']
     inlines = [CreativeInline]
+    form = GeoAdder
 
 @admin.register(Placement)
 class PlacementAdmin(admin.ModelAdmin):
     pass
 
+@admin.register(Creative)
+class CreativeAdmin(admin.ModelAdmin):
+    pass
