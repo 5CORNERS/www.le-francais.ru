dc77eb578745cda01823bdda9772e518e57b54d9
Anton Dumov <anton.dumov@gmail.com>
2022-11-14 15:20:27 +0300

    added exclude filter in errors admin page

--- log_errors/admin.py
+++ log_errors/admin.py
@@ -5,9 +5,22 @@ from log_errors.models import ExceptionLog
 
 # Register your models here.
 
+class ExcludeTypeFilter(admin.SimpleListFilter):
+    title = 'exclude type'
+    parameter_name = 'exclude'
+
+    def lookups(self, request, model_admin):
+        return (
+            ('http404', 'Http404'),
+        )
+
+    def queryset(self, request, queryset):
+        if self.value() == 'http404':
+            return queryset.exclude(type='Http404')
+
 @admin.register(ExceptionLog)
 class ExceptionLogAdmin(admin.ModelAdmin):
     list_display = ['datetime', 'type', 'path', 'value', 'user']
-    list_filter = ['type', 'datetime']
+    list_filter = [ExcludeTypeFilter, 'type', 'datetime']
     search_fields = ['type', 'path', 'user__username', 'user__email']
     readonly_fields = [field.name for field in ExceptionLog._meta.fields]
