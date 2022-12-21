82ef0556a6e378e033fb6cd53ac96caf543eeccb
Anton Dumov <anton.dumov@gmail.com>
2022-12-21 21:26:51 +0300

    added action to export csv from logs

--- ads/admin.py
+++ ads/admin.py
@@ -1,4 +1,7 @@
+import csv
+
 from django.contrib import admin
+from django.http import HttpResponse
 
 # Register your models here.
 from ads.forms import GeoAdder
@@ -51,6 +54,53 @@ class PlacementAdmin(admin.ModelAdmin):
 class CreativeAdmin(admin.ModelAdmin):
     pass
 
+def export_csv(modeladmin, request, queryset):
+    response = HttpResponse(
+        content_type='text/csv',
+    )
+    response['Content-Disposition'] = 'attachment; filename="export.csv"'
+    writer = csv.writer(response)
+    writer.writerow([
+        'Datetime',
+        'Clicked',
+        'Line Item',
+        'Creative Name',
+        'Creative Size',
+        'Ad Unit Name',
+        'Ad Unit Placements',
+        'Username',
+        'IP',
+        'Country',
+        'City',
+    ])
+    for log_object in queryset.select_related('line_item', 'creative'):
+        log_object: Log
+        if log_object.user is not None:
+            username = log_object.user.username
+        else:
+            username = ''
+        if log_object.ad_unit_placements:
+            placements_str = ", ".join(log_object.ad_unit_placements)
+        else:
+            placements_str = ''
+        writer.writerow([
+            log_object.datetime.strftime("%Y-%m-%d %H:%M:%S.%f"),
+            log_object.clicked,
+            log_object.line_item.name,
+            log_object.creative.name,
+            f'{log_object.creative.width}x{log_object.creative.height}',
+            log_object.ad_unit_name,
+            placements_str,
+            username,
+            log_object.ip,
+            log_object.country,
+            log_object.city
+        ])
+    return response
+
+export_csv.short_description = "Export CSV"
+
+
 @admin.register(Log)
 class LogAdmin(admin.ModelAdmin):
     list_display = [
@@ -64,3 +114,5 @@ class LogAdmin(admin.ModelAdmin):
     ]
     list_filter = ['line_item', 'datetime', 'clicked']
     readonly_fields = [field.name for field in Log._meta.fields]
+
+    actions = [export_csv]
