8cf965148da93485eb25f271ce6b965234d34a0c
Anton Dumov <anton.dumov@gmail.com>
2023-03-10 21:59:12 +0300

    cleaner ad sessions update

--- ads/management/commands/ads_clean_sessions.py
+++ ads/management/commands/ads_clean_sessions.py
@@ -11,16 +11,18 @@ class Command(BaseCommand):
     def handle(self, *args, **options):
         c = Session.objects.count()
         to_update = []
-        for i, session in enumerate(Session.objects.all().iterator()):
+        for i, session in enumerate(Session.objects.all().iterator(), start=1):
             session:Session
             data = session.get_decoded()
             clear_session_data(data)
             session.session_data = SessionStore().encode(data)
             if not 'ads_cappings' in data:
                 session.expire_date = session.last_activity + timedelta(days=7)
+            if len(data) == 1 and 'Yandex' in data:
+                session.expire_date = session.last_activity + timedelta(days=7)
             print(f'{i}/{c}', end='\r')
-            if i % 10000 == 0 or i == c:
-                print(f'Saving batch {i // 10000 }', end='... ')
+            if i % 100000 == 0 or i == c:
+                print(f'Saving batch {i // 100000 }', end='... ')
                 helper.bulk_update(to_update, update_fields=['data', 'expire_date'])
                 print('Done!')
                 to_update = []
