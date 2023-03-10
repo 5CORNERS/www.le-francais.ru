b739030169ec50b1914dc0882bab4e8be1313fde
Anton Dumov <anton.dumov@gmail.com>
2023-03-10 17:21:13 +0300

    migrating to render.com

--- le_francais/settings/base.py
+++ le_francais/settings/base.py
@@ -18,6 +18,7 @@ import sys
 import dj_database_url
 import dj_email_url
 from corsheaders.defaults import default_headers
+from django.core.management.utils import get_random_secret_key
 
 PROJECT_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 BASE_DIR = os.path.dirname(PROJECT_DIR)
