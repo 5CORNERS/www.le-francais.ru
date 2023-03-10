7e4e3349960d328f9e209ddc135699c5d1e6f126
Anton Dumov <anton.dumov@gmail.com>
2023-03-10 21:14:39 +0300

    cleaner ad sessions

--- ads/management/commands/ads_clean_sessions.py
+++ ads/management/commands/ads_clean_sessions.py
@@ -1,15 +1,27 @@
+from datetime import timedelta
+
 from django.core.management import BaseCommand
 
 from ads.utils import clear_session_data
 from user_sessions.models import Session
 from user_sessions.backends.db import SessionStore
+from django_bulk_update import helper
 
 class Command(BaseCommand):
     def handle(self, *args, **options):
         c = Session.objects.count()
-        for i, session_key in enumerate(Session.objects.values_list('session_key', flat=True)):
-            s = SessionStore(session_key=session_key)
+        to_update = []
+        for i, session in enumerate(Session.objects.all().iterator()):
+            session:Session
+            data = session.get_decoded()
+            clear_session_data(data)
+            session.session_data = SessionStore().encode(data)
+            if not 'ads_cappings' in data:
+                session.expire_date = session.last_activity + timedelta(days=7)
             print(f'{i}/{c}', end='\r')
-            clear_session_data(s)
-            s.save()
+            if i % 10000 == 0 or i == c:
+                print(f'Saving batch {i // 10000 }', end='... ')
+                helper.bulk_update(to_update, update_fields=['data', 'expire_date'])
+                print('Done!')
+                to_update = []
         print('\nDone!')
