cc1bcdcac3cc696719c421a4f65dc4453946995e
Anton Dumov <anton.dumov@gmail.com>
2023-03-17 15:53:02 +0300

    fix dotenv module error

--- manage.py
+++ manage.py
@@ -4,16 +4,20 @@ from __future__ import absolute_import, unicode_literals
 import os
 import sys
 
-from dotenv import load_dotenv
+try:
+    from dotenv import load_dotenv
+    os.environ.setdefault("DJANGO_SETTINGS_MODULE", 'le_francais.settings.dev')
+    dotenv_path = os.path.join(os.path.dirname(__file__), '.env')
+    load_dotenv(dotenv_path)
+except ModuleNotFoundError:
+    os.environ.setdefault("DJANGO_SETTINGS_MODULE", os.getenv('DJANGO_SETTINGS_MODULE'))
 
 sys.tracebacklimit = 30000
 
 if __name__ == "__main__":
-    os.environ.setdefault("DJANGO_SETTINGS_MODULE", "le_francais.settings.dev")
+
 
     from django.core.management import execute_from_command_line
 
-    dotenv_path = os.path.join(os.path.dirname(__file__), '.env')
-    load_dotenv(dotenv_path)
 
     execute_from_command_line(sys.argv)
