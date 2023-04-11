635576aa7749b404490232477161bf9194cb4754
Anton Dumov <anton.dumov@gmail.com>
2023-04-11 20:05:07 +0300

    added "time" argument

--- mass_mailer/management/commands/mass_mailer_send_by_name.py
+++ mass_mailer/management/commands/mass_mailer_send_by_name.py
@@ -1,3 +1,4 @@
+from freezegun import freeze_time
 from django.core.management import BaseCommand
 
 from mass_mailer.models import Message
@@ -10,7 +11,13 @@ class Command(BaseCommand):
         """
         parser.add_argument('-n', '--name', type=str, required=True,
                             help='Name of the message object', action='append')
+        parser.add_argument('-t', '--time', type=str, required=False,
+                            help='Set datetime', default=None)
     def handle(self, *args, **options):
+        if options['time'] is not None:
+            freezer = freeze_time(options['time'])
+            freezer.start()
+
         for name in options['name']:
             message = Message.objects.get(name=name)
             print(message.send())
