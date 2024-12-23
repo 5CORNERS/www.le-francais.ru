5b777d78e5baa9505ad8be9a5c75a03237a8d7fc
Anton Dumov <anton.dumov@gmail.com>
2024-12-23 13:28:38 +0200

    Added command to replace spaces with nbsp in LessonPage

---
+++ home/management/commands/replace_space_with_nbsp.py
@@ -0,0 +1,32 @@
+import re
+
+from django.core.management import BaseCommand
+
+from home.management.commands._private import set_block
+from home.models import LessonPage
+
+RE_REPLACE_SPACE_WITH_NBSP = [re.compile(r' (?=[!?;:»])'), re.compile(r'(?<=[«]) ')]
+RE_REPLACE_UNIX_LINE_BREAK = re.compile(r'\r\n')
+RE_ROW_START = re.compile(r'^-\s', re.MULTILINE)
+
+
+class Command(BaseCommand):
+    def handle(self, *args, **options):
+        for lesson in LessonPage.objects.all():
+            changed = False
+            for i in range(len(lesson.body.stream_data)):
+                block = lesson.body.__getitem__(i)
+                if block.block_type == 'paragraph':
+                    for pattern in RE_REPLACE_SPACE_WITH_NBSP:
+                        if pattern.search(block.value.source):
+                            block.value.source = re.sub(pattern, ' ', block.value.source)
+                            changed = True
+                    # if RE_REPLACE_UNIX_LINE_BREAK.search(block.value.source):
+                    #     print(lesson.lesson_number)
+                    # if RE_ROW_START.search(block.value.source):
+                    #     # print(lesson.lesson_number)
+                    #     pass
+                    set_block(i, block, lesson.body)
+            if changed:
+                print(lesson.lesson_number)
+                lesson.save_revision().publish()
