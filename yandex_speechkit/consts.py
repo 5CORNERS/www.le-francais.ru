dd77032f6ad64375c579b944280099cb7bc80b40
Anton Dumov <anton.dumov@gmail.com>
2025-02-24 12:43:33 +0200

    "Added support for multiple audio formats and sample rates"

--- yandex_speechkit/consts.py
+++ yandex_speechkit/consts.py
@@ -15,38 +15,60 @@ LANGUAGE_CHOICES = [
     (LANGUAGE_RU, 'Russian')
 ]
 
+VOICE_ALENA = 'alena'
+VOICE_FILIPP = 'filipp'
+
 VOICES_CHOICES = [
-    ('alena', 'alena'),
-    ('filipp', 'filipp')
+    (VOICE_ALENA, 'alena'),
+    (VOICE_FILIPP, 'filipp')
 ]
 
+EMOTION_NEUTRAL = 'neutral'
+EMOTION_WHISPER = 'whisper'
+EMOTION_FRIENDLY = 'friendly'
+EMOTION_GOOD = 'good'
+EMOTION_STRICT = 'strict'
+EMOTION_EVIL = 'evil'
+
 EMOTION_CHOICES = [
-    ('neutral','neutral',),
-    ('whisper','whisper',),
-    ('friendly','friendly',),
-    ('good','good',),
-    ('strict','strict',),
-    ('evil','evil'),
+    (EMOTION_NEUTRAL, 'neutral',),
+    (EMOTION_WHISPER, 'whisper',),
+    (EMOTION_FRIENDLY, 'friendly',),
+    (EMOTION_GOOD, 'good',),
+    (EMOTION_STRICT, 'strict',),
+    (EMOTION_EVIL, 'evil'),
 ]
 
 FORMAT_MP3 = 'mp3'
+FORMAT_LPCM = 'lpcm'
+FORMAT_OGGOPUS = 'oggopus'
 
 FORMAT_CHOICES = [
-    ('lpcm', 'lpcm',),
-    ('oggopus', 'oggopus',),
+    (FORMAT_LPCM, 'lpcm',),
+    (FORMAT_OGGOPUS, 'oggopus',),
     (FORMAT_MP3, 'mp3',),
 ]
 
+DEFAULT_VOICE_FEMALE = VOICE_ALENA
+DEFAULT_EMOTION_FEMALE = EMOTION_NEUTRAL
+DEFAULT_VOICE_MALE = VOICE_FILIPP
+DEFAULT_EMOTION_MALE = EMOTION_NEUTRAL
+DEFAULT_FORMAT = FORMAT_LPCM
+
 FILE_EXTENSION = {
     FORMAT_MP3: 'mp3',
-    'lpcm': 'wav',
-    'oggopus': 'ogg',
+    FORMAT_LPCM: 'wav',
+    FORMAT_OGGOPUS: 'ogg',
 }
 
+SAMPLE_RATE_8000 = '8000'
+SAMPLE_RATE_16000 = '16000'
+SAMPLE_RATE_48000 = '48000'
+
 SAMPLE_RATE_CHOICES = [
-    ('8000','8000',),
-    ('16000','16000',),
-    ('48000','48000',),
+    (SAMPLE_RATE_8000, '8000',),
+    (SAMPLE_RATE_16000, '16000',),
+    (SAMPLE_RATE_48000, '48000',),
 ]
 
-FILES_LE_FRANCAIS_PATH = '/speechkit/'
+FILES_LE_FRANCAIS_PATH = 'dictionary/speechkit/'
--- yandex_speechkit/models.py
+++ yandex_speechkit/models.py
@@ -8,11 +8,12 @@ from django.db import models
 from django.utils.text import slugify
 
 from os import environ, pathsep, path
+
 environ["PATH"] += pathsep + path.abspath(os.path.join(settings.BASE_DIR, "ffmpeg"))
 from pydub import AudioSegment
 
 from yandex_speechkit.consts import LANGUAGE_CHOICES, VOICES_CHOICES, EMOTION_CHOICES, FORMAT_CHOICES, \
-    SAMPLE_RATE_CHOICES, PARAMS, FORMAT_MP3, FILES_LE_FRANCAIS_PATH, FILE_EXTENSION
+    SAMPLE_RATE_CHOICES, PARAMS, FORMAT_LPCM, FILES_LE_FRANCAIS_PATH, FILE_EXTENSION, SAMPLE_RATE_48000, FORMAT_MP3
 from yandex_speechkit.api import YandexAPI
 
 
@@ -24,28 +25,45 @@ class YandexSpeechKitTask(models.Model):
     lang = models.CharField(max_length=10, choices=LANGUAGE_CHOICES, null=True, default=None)
     voice = models.CharField(max_length=10, choices=VOICES_CHOICES, null=True, default=None)
     emotion = models.CharField(max_length=10, choices=EMOTION_CHOICES, null=True, default=None)
-    format = models.CharField(max_length=10, choices=FORMAT_CHOICES, null=False, default=FORMAT_MP3)
-    sample_rate = models.CharField(max_length=10, choices=SAMPLE_RATE_CHOICES, null=True, default=None, blank=True)
+    format = models.CharField(max_length=10, choices=FORMAT_CHOICES, null=False, default=FORMAT_LPCM)
+    sample_rate = models.CharField(max_length=10, choices=SAMPLE_RATE_CHOICES, null=True, default=SAMPLE_RATE_48000,
+                                   blank=True)
     task_status = models.CharField(max_length=10, null=True, default='CREATED')
     error = models.BooleanField(default=False, blank=True)
     stream = models.BinaryField(null=True, default=None, blank=True)
     url = models.URLField(null=True, default=None, blank=True)
     error_message = models.TextField(null=True, default=None, blank=True)
 
-    def get_filename(self):
+    def get_filename(self, extension=None) -> str:
         if self.text:
             name = slugify(self.text, allow_unicode=True)
         else:
             name = slugify(re.sub(r"</?[^>]+>", "", self.ssml), allow_unicode=True)
-        return f"{self.pk}_{name}.{FILE_EXTENSION[self.format]}"
+        if extension is None:
+            extension = FILE_EXTENSION[self.format]
+        return f"{self.pk}_{name}.{extension}"
 
-    def get_file(self):
+    def get_file(self) -> BytesIO:
         file = BytesIO()
         file.write(self.stream)
         file.seek(0)
         return file
 
-    def to_dict(self):
+    def get_segment(self) -> AudioSegment:
+        file = self.get_file()
+        if self.format == FORMAT_MP3:
+            return AudioSegment.from_file(file, 'mp3')
+        elif self.format == FORMAT_LPCM:
+            return AudioSegment.from_raw(
+                file,
+                frame_rate=int(self.sample_rate),
+                channels=1,
+                sample_width=2,
+            )
+        else:
+            return AudioSegment.from_file(file, format=FILE_EXTENSION[self.format])
+
+    def to_dict(self) -> dict:
         opts = self._meta
         data = {}
         for f in opts.concrete_fields:
@@ -53,7 +71,7 @@ class YandexSpeechKitTask(models.Model):
                 data[PARAMS[f.name]] = f.value_from_object(self)
         return data
 
-    def synthesize(self):
+    def synthesize(self) -> 'YandexSpeechKitTask':
         if self.pk is None:
             self.save()
         api = YandexAPI()
@@ -62,10 +80,16 @@ class YandexSpeechKitTask(models.Model):
             self.stream = stream.read()
         else:
             self.stream = None
-        self.save(update_fields=['stream', 'error', 'task_status'])
+        self.save(update_fields=['stream', 'error', 'task_status', 'error_message'])
+        return self
 
-    def save_to_file(self):
-        path = f'/var/www/www-root/data/www/files.le-francais.ru{FILES_LE_FRANCAIS_PATH}'
+    def save_to_file(self, save_path=None, tags=None, result_task_status='saved'):
+        if save_path is None:
+            url = f"https://files.le-francais.ru/dictionary/speechkit/{self.get_filename(extension='mp3')}"
+            save_path = f'/var/www/www-root/data/www/files.le-francais.ru/{FILES_LE_FRANCAIS_PATH}'
+        else:
+            url = f"https://files.le-francais.ru/{save_path}{self.get_filename(extension='mp3')}"
+            save_path = f'/var/www/www-root/data/www/files.le-francais.ru/{save_path}'
         cnopts = pysftp.CnOpts()
         cnopts.hostkeys = None
         srv = pysftp.Connection(
@@ -74,18 +98,18 @@ class YandexSpeechKitTask(models.Model):
             password=os.environ.get('SFTP_FILES_LE_FRANCAIS_PASSWORD'),
             cnopts=cnopts
         )
-        with srv.cd(path):
-            filename = self.get_filename()
-            with BytesIO() as file:
-                file.write(self.stream)
-                file.seek(0)
-                srv.putfo(file, filename)
-            self.url = f"https://files.le-francais.ru{FILES_LE_FRANCAIS_PATH}{filename}"
+        srv.makedirs(save_path)
+        with srv.cd(save_path):
+            filename = self.get_filename(extension='mp3')
+            file = self.get_mp3(tags)
+            srv.putfo(file, filename)
+            self.url = url,
             self.stream = None
-            self.status = 'SAVED'
-            self.save(update_fields=['url', 'stream'])
+            self.task_status = result_task_status
+            self.save(update_fields=['url', 'stream', 'task_status'])
+        return self
 
-    def start_task(self):
+    def start_task(self) -> None:
         try:
             self.synthesize()
             self.save_to_file()
@@ -95,13 +119,32 @@ class YandexSpeechKitTask(models.Model):
             self.save(update_fields=['error', 'task_status'])
             raise e
 
+    def get_mp3(self, tags) -> BytesIO:
+        file = BytesIO()
+        if self.format == FORMAT_MP3:
+            file.write(self.stream)
+        else:
+            if self.format == FORMAT_LPCM:
+                audio_segment = AudioSegment.from_file(
+                    self.get_file(),
+                    'raw',
+                    frame_rate=int(self.sample_rate),
+                    channels=1,
+                    sample_width=2,
+                )
+            else:
+                audio_segment = AudioSegment.from_file(self.get_file(), format=FILE_EXTENSION[self.format])
+            audio_segment.export(file, format="mp3", tags=tags if tags else {}, bitrate="192k")
+        file.seek(0)
+        return file
+
 
 def combine_audio_streams(file1, file2, export_format="mp3"):
     stream1 = AudioSegment.from_mp3(file1)
     stream2 = AudioSegment.from_mp3(file2)
     combined_stream = stream1 + stream2
     output = BytesIO()
-    combined_stream.export(output, format=export_format)
+    combined_stream.export(output, format=export_format, bitrate="192k")
     output.seek(0)  # Reset the stream position
     return output.read()
 
@@ -123,4 +166,32 @@ def create_joined_task(primary_task, secondary_task):
 
     # Save the task and return
     new_task.save()
+    return new_task
+
+
+def create_joined_task_v2(*tasks, params, text=None, tags=None, save_path=None):
+    segments = []
+    for task, param in zip(tasks, params):
+        task: YandexSpeechKitTask
+        segment = task.get_segment()
+        if 'level' in param.keys() and param['level']:
+            segment = segment - 6
+        segments.append(segment)
+
+    result: AudioSegment = segments[0]
+    for segment in segments[1:]:
+        result += segment
+
+    new_task = YandexSpeechKitTask()
+    for attr in ["lang", "voice", "emotion", "format", "sample_rate"]:
+        setattr(new_task, attr, getattr(tasks[0], attr))
+    if text:
+        new_task.text = text
+    with BytesIO() as new_file:
+        result.export(new_file, format="wav")
+        new_file.seek(0)
+        new_task.stream = new_file.read()
+
+    new_task.save()
+    new_task.save_to_file(save_path, tags=tags)
     return new_task
