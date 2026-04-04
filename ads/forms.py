de277c0653667bf8fc4155d3b3758b78bcba72e5
Anton Dumov <anton.dumov@gmail.com>
2026-04-04 19:45:44 +0300

    refactor: improve form compatibility and query handling

--- ads/forms.py
+++ ads/forms.py
@@ -1,33 +1,60 @@
 from django.contrib.auth import get_user_model
-from django.db.models import Count
+from django.db.models import Count, Q
 from django import forms
-from django.utils.encoding import force_text
+
+# Forward compatibility for modern Django
+try:
+    from django.utils.encoding import force_str
+except ImportError:
+    from django.utils.encoding import force_text as force_str
 
 from ads.models import LineItem
-
 User = get_user_model()
-
-COUNTRIES_CHOICES = [(x[0], x[1]) for x in User.objects.exclude(country_code__isnull=True).values_list('country_code', 'country_name').annotate(count=Count('country_code')).order_by('-count')]
-CITIES_CHOICES = [(x[0], x[0]) for x in User.objects.exclude(city__isnull=True).values_list('city').annotate(count=Count('city')).order_by('-count')]
-
 
 class ArrayMultipleSelected(forms.SelectMultiple):
     def format_value(self, value):
-        if value is None and self.allow_multiple_selected:
+        if value is None:
             return []
+
         if not isinstance(value, (tuple, list)):
-            # This means it should be a comma delimited list of items so parse it
-            value = value.split(',')
-        return [force_text(v) if v is not None else '' for v in value]
+            # Safely verify it's a string before splitting
+            if isinstance(value, str):
+                value = value.split(',')
+            else:
+                value = [value]
+
+        return [force_str(v) if v is not None else '' for v in value]
+
 
 class GeoAdder(forms.ModelForm):
     def __init__(self, *args, **kwargs):
         super(GeoAdder, self).__init__(*args, **kwargs)
+
+        # 1. Queries evaluate dynamically when form is created
+        # 2. Excludes empty strings ("") along with NULLs
+        countries_choices = [
+            (x[0], x[1]) for x in User.objects.exclude(
+                Q(country_code__isnull=True) | Q(country_code__exact='')
+            ).values_list('country_code', 'country_name').annotate(
+                count=Count('country_code')
+            ).order_by('-count')
+        ]
+
+        cities_choices = [
+            (x[0], x[0]) for x in User.objects.exclude(
+                Q(city__isnull=True) | Q(city__exact='')
+            ).values_list('city').annotate(
+                count=Count('city')
+            ).order_by('-count')
+        ]
+
         self.fields['targeting_countries'].widget = ArrayMultipleSelected(
-            attrs={'class': 'multi-select-input'}, choices=COUNTRIES_CHOICES
+            attrs={'class': 'multi-select-input'},
+            choices=countries_choices
         )
         self.fields['targeting_cities'].widget = ArrayMultipleSelected(
-            attrs={'class': 'multi-select-input'}, choices=CITIES_CHOICES
+            attrs={'class': 'multi-select-input'},
+            choices=cities_choices
         )
 
     class Meta:
